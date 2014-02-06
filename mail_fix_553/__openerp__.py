f324eb8e0a39b39ce48a2b528044db3a97c06c1e
Ivan Yelizariev <ivann.exe@gmail.com>
2014-02-06 14:42:15 +0600

    update description

--- mail_fix_553/__openerp__.py
+++ mail_fix_553/__openerp__.py
@@ -6,17 +6,17 @@
     "website" : "https://it-projects.info",
     "description": """Update 'Reply-to' field to catchall value in order to fix problem like that:
 
-    2014-01-18 06:25:56,532 6789 INFO trunk openerp.addons.mail.mail_thread: Routing mail from <MYLOGIN@yandex.ru> to admin@MYDOMAIN.com with Message-Id <49131390026345@web16h.yandex.ru>: direct alias match: (u'res.users', 1, {}, 1, browse_record(mail.alias, 1))
+    2014-01-18 06:25:56,532 6789 INFO trunk openerp.addons.mail.mail_thread: Routing mail from <user@CUSTOMER.com> to info@MYDOMAIN.com with Message-Id <49131390026345@web16h.yandex.ru>: direct alias match: (u'res.users', 1, {}, 1, browse_record(mail.alias, 1))
 2014-01-18 06:25:57,212 6789 ERROR trunk openerp.addons.base.ir.ir_mail_server: Mail delivery failed via SMTP server 'smtp.yandex.ru'.
 SMTPSenderRefused: 553
 5.7.1 Sender address rejected: not owned by auth user.
-MYLOGIN@yandex.ru
+user@CUSTOMER.com
 Traceback (most recent call last):
   File "/mnt/files/src/openerp-server/server/openerp/addons/base/ir/ir_mail_server.py", line 465, in send_email
     smtp.sendmail(smtp_from, smtp_to_list, message.as_string())
   File "/usr/lib/python2.7/smtplib.py", line 722, in sendmail
     raise SMTPSenderRefused(code, resp, from_addr)
-SMTPSenderRefused: (553, '5.7.1 Sender address rejected: not owned by auth user.', 'MYLOGIN@yandex.ru')
+SMTPSenderRefused: (553, '5.7.1 Sender address rejected: not owned by auth user.', 'user@CUSTOMER.com')
 
 2014-01-18 06:25:57,216 6789 ERROR trunk openerp.addons.mail.mail_mail: failed sending mail.mail 2
 Traceback (most recent call last):
@@ -24,7 +24,7 @@ Traceback (most recent call last):
     context=context)
   File "/mnt/files/src/openerp-server/server/openerp/addons/base/ir/ir_mail_server.py", line 478, in send_email
     raise MailDeliveryException(_("Mail Delivery Failed"), msg)
-MailDeliveryException: (u'Mail Delivery Failed', u"Mail delivery failed via SMTP server 'smtp.yandex.ru'.\nSMTPSenderRefused: 553\n5.7.1 Sender address rejected: not owned by auth user.\nMYLOGIN@yandex.ru")
+MailDeliveryException: (u'Mail Delivery Failed', u"Mail delivery failed via SMTP server 'smtp.yandex.ru'.\nSMTPSenderRefused: 553\n5.7.1 Sender address rejected: not owned by auth user.\nuser@CUSTOMER.com")
 2014-01-18 06:25:57,223 6789 INFO trunk openerp.addons.fetchmail.fetchmail: fetched/processed 1 email(s) on imap server yandex
 
     """,
