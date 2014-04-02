2816dd79117003b948b974b80b3a2cd9b96073be
Ivan Yelizariev <ivann.exe@gmail.com>
2014-04-02 16:35:34 +0600

    upload sale_order_report_ru

---
+++ sale_order_report_ru/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    "name" : "Sale order report in russian ",
+    "version" : "0.1",
+    "author" : "Ivan Yelizariev",
+    "category" : "Sale",
+    "website" : "https://it-projects.info",
+    "description": """
+    """,
+    "depends" : ["sale"],
+    "data":[
+        'report.xml',
+        ],
+    "installable": True
+}
