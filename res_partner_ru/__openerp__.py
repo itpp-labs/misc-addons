74f6d4e91cd5b01a2f409ad5701b98bc577ec310
Ivan Yelizariev <ivann.exe@gmail.com>
2014-04-02 10:18:16 +0600

    res_partner_ru module

---
+++ res_partner_ru/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Partner info extension",
+    "version" : "0.1",
+    "author" : "Ivan Yelizariev",
+    "category" : "Sale",
+    "website" : "https://it-projects.info",
+    "description": """
+    Добавляет ИНН\КПП, расчетный счет в виджет contact
+
+    """,
+    "depends" : ['website'],
+    "data":[
+        'templates.xml'
+        ],
+    "installable": True
+}
