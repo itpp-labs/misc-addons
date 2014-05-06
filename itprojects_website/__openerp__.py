4573c29df689aa02aae3bb7bbcb848000b980e1a
Ivan Yelizariev <ivann.exe@gmail.com>
2014-05-06 18:41:22 +0600

    upload itprojects_website

---
+++ itprojects_website/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    "name" : "Website updates for It-Projects LLC ",
+    "version" : "0.1",
+    "author" : "Ivan Yelizariev",
+    "category" : "Website",
+    "website" : "https://it-projects.info",
+    "description": """
+    """,
+    "depends" : ["website"],
+    "data":[
+        'templates.xml',
+        ],
+    "installable": True
+}
