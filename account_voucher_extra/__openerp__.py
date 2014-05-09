06c6db7217c958bf4ada499240abbd8ec75a9ccf
Ivan Yelizariev <ivann.exe@gmail.com>
2014-05-09 13:55:55 +0600

    upload account_voucher_extra

---
+++ account_voucher_extra/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Account Voucher extension",
+    "version" : "0.1",
+    "author" : "Ivan Yelizariev",
+    "category" : "Sale",
+    "website" : "https://it-projects.info",
+    "description": """
+
+
+    """,
+    "depends" : ['account_voucher'],
+    "data":[
+        'views.xml'
+        ],
+    "installable": True
+}
