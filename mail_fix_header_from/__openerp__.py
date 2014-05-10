74effdcaf25ee6b9d5f3928d726375a8478e94ca
Ivan Yelizariev <ivann.exe@gmail.com>
2014-05-10 18:19:21 +0600

    upload mail_fix_header_from

---
+++ mail_fix_header_from/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    "name" : "Fix non-ascii header 'from'",
+    "version" : "0.3",
+    "author" : "Ivan Yelizariev",
+    "category" : "Mail",
+    "website" : "https://it-projects.info",
+    "description": """
+    """,
+    "depends" : ["base"],
+    #"init_xml" : [],
+    #"update_xml" : [],
+    #"active": True,
+    "installable": True
+}
