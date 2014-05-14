390a21390fa16e56b38e653874e78f26a7b10dd8
Ivan Yelizariev <ivann.exe@gmail.com>
2014-05-14 12:48:32 +0600

    upload project_description

---
+++ project_description/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+{
+    "name" : "Add Project Description",
+    "version": "1.0",
+    "author" : "Julius Network Solutions",
+    "website" : "http://www.julius.fr",
+    "category" : "Generic Modules/Projects & Services",
+    "depends" : ["project",
+                 ],
+    "description": """Add Project Description""",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml": [
+        "project_view.xml", 
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ project_description/project.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+class project_project(orm.Model):
+    _inherit = "project.project"
+    _columns = {
+        'description': fields.text('description'),
+    }
+    
+class account_analytic_account(orm.Model):
+    _inherit = 'account.analytic.account'
+    
+    def name_get(self, cr, uid, ids, context=None):
+        res = []
+        if not ids:
+            return res
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        for id in ids:
+            elmt = self.browse(cr, uid, id, context=context)
+            res.append((id, elmt.name))
+        return res
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
