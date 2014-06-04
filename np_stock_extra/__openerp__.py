ceffacd975591399649bce5d39980ae19d9217ab
Ivan Yelizariev <ivann.exe@gmail.com>
2014-06-04 10:35:06 +0600

    upload np_stock_extra

---
+++ np_stock_extra/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'stock updates for NP company',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Base',
+    'website' : 'https://it-projects.info',
+    'description': """
+    """,
+    'depends' : ['stock', 'np_stock_check_quantity'],
+    'data':[
+        'views.xml'
+        ],
+    'demo':[
+    ],
+    'installable': True
+}
