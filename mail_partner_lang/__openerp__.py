b36072fcc3789118d6210a9d7df77b5dfb7f8b01
Ivan Yelizariev <ivann.exe@gmail.com>
2014-07-11 12:21:20 +0600

    new module mail_partner_lang

---
+++ mail_partner_lang/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name' : 'Use partner language in mail',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Mail',
+    'website' : 'https://it-projects.info',
+
+    'depends' : ['mail'],
+    'data':[],
+    'installable': True,
+    'description': '''
+    ''',
+}
