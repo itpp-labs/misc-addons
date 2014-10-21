bf22ab39573278938c4922277ba3e1b87d3f812c
Ivan Yelizariev <ivann.exe@gmail.com>
2014-10-21 10:28:40 +0200

    upload custom_menu_bar

---
+++ custom_menu_bar/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Custom menu bar (orange)',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Base',
+    'website' : 'https://it-projects.info',
+    'description': """
+
+    """,
+    'depends' : ['base'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
