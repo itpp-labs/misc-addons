9ca773b5789f1f3df0bf69543bbe31ee80fdec46
Ivan Yelizariev <ivann.exe@gmail.com>
2014-11-14 17:45:33 +0200

    [IMP] improve waybill

--- sale_report_kz/__openerp__.py
+++ sale_report_kz/__openerp__.py
@@ -11,7 +11,7 @@
     depends on https://github.com/j2a/pytils
     tested on 8.0 29e08a272c0add31086f7c30ffe154a63e2edf24
     """,
-    "depends" : ["stock", "sale_report_ru"],
+    "depends" : ["stock", "sale_report_ru", "l10n_ru"],
     "data":[
         'report.xml',
         ],
