b37d9af57837a772db4ae063deb741b1b9d41fae
Ivan Yelizariev <ivann.exe@gmail.com>
2014-11-18 13:51:45 +0200

    [IMP] hide 'Personal Information' and 'HR Settings' tabs in employee form if user not in special groups and this is not his form

--- access_custom/models.py
+++ access_custom/models.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv,fields
+from openerp.osv import fields as old_fields
+from openerp.osv import osv
+from openerp import api, models, fields
 from openerp import SUPERUSER_ID
 from openerp.tools.translate import _
 from openerp.exceptions import AccessError
 
-class hr_employee(osv.osv):
+class hr_employee(models.Model):
     '''
     Employee
     '''
@@ -12,7 +14,6 @@ class hr_employee(osv.osv):
     _inherit = 'hr.employee'
 
     def _payslip_count(self, cr, uid, ids, field_name, arg, context=None):
-        print '_payslip_count'
         try:
             res = super(hr_employee, self)._payslip_count(cr, uid, ids, field_name, arg, context)
         except AccessError, e:
@@ -22,5 +23,10 @@ class hr_employee(osv.osv):
             }
         return res
     _columns = {
-        'payslip_count': fields.function(_payslip_count, type='integer', string='Payslips'),
+        'payslip_count': old_fields.function(_payslip_count, type='integer', string='Payslips'),
     }
+
+    def _get_access_to_employee_information(self):
+        self.access_to_employee_information = (self.user_id.id == self.env.uid) or (self.env.ref('access_custom.group_employee_information').id in self.env.user.groups_id.ids)
+
+    access_to_employee_information = fields.Boolean('Access to employee information', compute=_get_access_to_employee_information, store=False)
