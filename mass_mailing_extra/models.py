dc76eee9ac17cbe92aab914c90c09dd2e8bf792d
Ivan Yelizariev <ivann.exe@gmail.com>
2014-11-25 12:34:35 +0200

    [IMP] get partner from Document ID

--- mass_mailing_extra/models.py
+++ mass_mailing_extra/models.py
@@ -4,13 +4,25 @@ class MailMailStats(models.Model):
 
     _inherit = 'mail.mail.statistics'
 
-    partner_ids = fields.Many2many('res.partner', related='mail_mail_id.recipient_ids', string='Partners')
+    partner_ids = fields.Many2many('res.partner', related='mail_mail_id.recipient_ids', string='Partners (Mail)')
 
     @api.one
-    def _get_partner_ids_text(self):
-        res = []
-        for p in self.partner_ids:
-            res.append('%s <%s>' % (p.name, p.email))
-        self.partner_ids_text = ', '.join(res)
+    def _get_partner_id(self):
+        if self.model=='res.partner':
+            self.partner_id = self.res_id
+        else:
+            self.partner_id = None
 
-    partner_ids_text = fields.Char('Partners', compute=_get_partner_ids_text)
+    partner_id = fields.Many2one('res.partner', compute=_get_partner_id, string='Partner (Document ID)')
+
+    @api.one
+    def _get_partners(self):
+        res = {}
+        for p in self.partner_ids:
+            res[p.id] = p
+        if self.partner_id and self.partner_id.id not in res:
+            res[self.partner_id.id] = self.partner_id
+        self.partners = ', '.join([('%s <%s>' % (p.name, p.email)) for id,p in res.items()])
+
+
+    partners = fields.Char('Partners', compute=_get_partners)
