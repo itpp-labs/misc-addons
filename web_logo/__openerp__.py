681bded41e4cac27be5d77fcaa40a94b02fc2920
Ivan Yelizariev <ivann.exe@gmail.com>
2014-11-29 17:12:51 +0200

    [IMP] upload web_logo

---
+++ web_logo/__openerp__.py
@@ -0,0 +1,23 @@
+{
+    'name' : 'Logo improvements for multicompany',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Base',
+    'website' : 'https://it-projects.info',
+    'description': """
+
+Module adds "?company_id=123" to logo url in order to:
+
+* prevent using wrong cached logo
+* show correct logo for public users (company_id qcontext variable should be specified)
+
+ Tested on odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : ['website', 'web'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ web_logo/controllers/main.py
@@ -0,0 +1,60 @@
+import openerp
+from openerp import http, SUPERUSER_ID
+from openerp.addons.web.controllers.main import Binary
+import functools
+from openerp.http import request, serialize_exception as _serialize_exception
+from openerp.modules import get_module_resource
+from cStringIO import StringIO
+
+
+@http.route([
+    '/web/binary/company_logo',
+    '/logo',
+    '/logo.png',
+], type='http', auth="none")
+def company_logo(self, dbname=None, company_id=None, **kw):
+    print 'company_id', company_id
+    imgname = 'logo.png'
+    placeholder = functools.partial(get_module_resource, 'web', 'static', 'src', 'img')
+    uid = None
+    if request.session.db:
+        dbname = request.session.db
+        uid = request.session.uid
+    elif dbname is None:
+        dbname = db_monodb()
+
+    if not uid:
+        uid = openerp.SUPERUSER_ID
+
+    if not dbname:
+        response = http.send_file(placeholder(imgname))
+    else:
+        try:
+            # create an empty registry
+            registry = openerp.modules.registry.Registry(dbname)
+            with registry.cursor() as cr:
+                if company_id:
+                    cr.execute("""SELECT c.logo_web, c.write_date
+                                    FROM res_company c
+                                   WHERE c.id = %s
+                           """, (company_id,))
+                else:
+                    cr.execute("""SELECT c.logo_web, c.write_date
+                                    FROM res_users u
+                               LEFT JOIN res_company c
+                                      ON c.id = u.company_id
+                                   WHERE u.id = %s
+                           """, (uid,))
+                row = cr.fetchone()
+                if row and row[0]:
+                    image_data = StringIO(str(row[0]).decode('base64'))
+                    response = http.send_file(image_data, filename=imgname, mtime=row[1])
+                else:
+                    response = http.send_file(placeholder('nologo.png'))
+        except Exception, e:
+            raise e
+            response = http.send_file(placeholder(imgname))
+
+    return response
+
+Binary.company_logo = company_logo
