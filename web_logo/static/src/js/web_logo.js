a6ebb9c81b0d8afd67f2b79843c4781e557f46cc
Ivan Yelizariev <ivann.exe@gmail.com>
2014-12-16 21:42:53 +0200

    [FIX] UserMenu

--- web_logo/static/src/js/web_logo.js
+++ web_logo/static/src/js/web_logo.js
@@ -6,28 +6,14 @@ openerp.web_logo = function(instance){
 
 
     do_update: function () {
+        this._super.apply(this, arguments);
         var self = this;
         var fct = function() {
-            var $avatar = self.$el.find('.oe_topbar_avatar');
-            $avatar.attr('src', $avatar.data('default-src'));
             if (!self.session.uid)
                 return;
             var func = new instance.web.Model("res.users").get_func("read");
             return self.alive(func(self.session.uid, ["name", "company_id"])).then(function(res) {
-                var topbar_name = res.name;
-                if(instance.session.debug)
-                    topbar_name = _.str.sprintf("%s (%s)", topbar_name, instance.session.db);
                 $('.oe_logo img').attr('src', '/web/binary/company_logo?company_id=' + res.company_id[0]);
-                if(res.company_id[0] > 1)
-                    topbar_name = _.str.sprintf("%s (%s)", topbar_name, res.company_id[1]);
-                self.$el.find('.oe_topbar_name').text(topbar_name);
-                if (!instance.session.debug) {
-                    topbar_name = _.str.sprintf("%s (%s)", topbar_name, instance.session.db);
-                }
-                var avatar_src = self.session.url('/web/binary/image', {model:'res.users', field: 'image_small', id: self.session.uid});
-                $avatar.attr('src', avatar_src);
-
-                openerp.web.bus.trigger('resize');  // Re-trigger the reflow logic
             });
         };
         this.update_promise = this.update_promise.then(fct, fct);
