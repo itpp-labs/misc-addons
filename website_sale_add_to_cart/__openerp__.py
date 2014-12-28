263cf4fd56e64b3253cda8eb3defb8f0eee2e4b4
Ivan Yelizariev <ivann.exe@gmail.com>
2014-12-28 21:11:12 +0200

    [IMP] doc

--- website_sale_add_to_cart/__openerp__.py
+++ website_sale_add_to_cart/__openerp__.py
@@ -6,6 +6,15 @@
     'website' : 'https://it-projects.info',
     'description': """
 
+Adds
+
+    [-] 0[+]
+
+buttons to the products cell (near the price). When customer click "+" or type count at the input field a product will added immediatly.
+
+and on the product page removes "Add to Cart" button
+and add the same buttons to show current count of a products in the cart. When customer click "+" or type count at the input field a product will added immediatly.
+
 TODO don't ignore multivariant products
 
 Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284
