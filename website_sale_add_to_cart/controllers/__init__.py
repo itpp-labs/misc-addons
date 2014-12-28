30995df10179015892ff5bc2816bc9e7e285680d
Ivan Yelizariev <ivann.exe@gmail.com>
2014-12-28 18:10:46 +0200

    website_sale_add_to_cart

---
+++ website_sale_add_to_cart/controllers/__init__.py
@@ -0,0 +1,1 @@
+import website_sale_add_to_cart
---
+++ website_sale_add_to_cart/static/src/js/website_sale_add_to_cart.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    if (!$('input.product_id') || !$('#products_grid_before'))
+        return;
+    $(".oe_website_sale input.js_quantity").change(function () {
+        var $input = $(this);
+        var value = parseInt($input.val(), 10);
+        if (isNaN(value)) value = 0;
+        openerp.jsonRpc("/shop/cart/update_json", 'call', {
+            'line_id': parseInt($input.data('line-id'),10),
+            'product_id': parseInt($input.data('product-id') || $('input.product_id').val(),10),
+            'set_qty': value})
+            .then(function (data) {
+                if (!data.quantity) {
+                    location.reload();
+                    return;
+                }
+                var $q = $(".my_cart_quantity");
+                $q.parent().parent().removeClass("hidden", !data.quantity);
+                $q.html(data.cart_quantity).hide().fadeIn(600);
+                $input.val(data.quantity);
+                $("#cart_total").replaceWith(data['website_sale.total']);
+            });
+    });
+    /*
+    $('.oe_website_sale .oe_product_cart .oe_product_image a').on('click', function (ev) {
+        //ev.preventDefault();
+        var $input = $(ev.currentTarget).parent().parent().find("input");
+        $input.val(1 + parseFloat($input.val(),10));
+        $input.change();
+        $input.css('background-color', '#428bca').stop().animate({backgroundColor:'white'}, 500)
+        return false;
+    });
+    */
+
+    openerp.jsonRpc("/shop/get_order_numbers", 'call').then(function(data){
+        if (!data)
+            return;
+        var page_product_id = $('input.product_id').val();//for page of a product
+        $.each(data, function(product_id, num){
+            if (page_product_id){
+                if (page_product_id == product_id){
+                    $('input.js_quantity').val(num);
+                }
+            } else
+                $('input[data-product-id="'+product_id+'"]').val(num)
+        })
+    })
+
+})
