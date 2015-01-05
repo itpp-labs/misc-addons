ee9bbba2b3597125bd318261f11567fa68797831
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-05 12:47:40 +0200

    [FIX] access issue

--- website_sale_special_offer/__openerp__.py
+++ website_sale_special_offer/__openerp__.py
@@ -12,6 +12,7 @@ Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284.
     'depends' : ['website_sale'],
     'data':[
         'website_sale_special_offer_views.xml',
+        'security/ir.model.access.csv',
         ],
     'installable': True
 }
