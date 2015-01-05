db0c0c3782c80a3fc9b156a0bd8562f055c5d0dc
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-05 19:06:57 +0200

    integrate website_sale_special_offer with website_sale_clear_cart

--- website_sale_special_offer/__openerp__.py
+++ website_sale_special_offer/__openerp__.py
@@ -7,9 +7,11 @@
     'description': """
 Module allows to create special offer on a web shop.
 
+This module depends on website_sale_clear_cart module, because it's only way to delete mandatory products from /shop/cart page.
+
 Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284.
     """,
-    'depends' : ['website_sale'],
+    'depends' : ['website_sale', 'website_sale_clear_cart'],
     'data':[
         'website_sale_special_offer_views.xml',
         'security/ir.model.access.csv',
