43a3295c9d388efdee2c3074a49cc32e9cfbb77e
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-12 10:53:31 +0200

    upload res_users_hide_suggestions

---
+++ res_users_hide_suggestions/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Hide suggestions',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+Set False as default value for display_employees_suggestions and display_groups_suggestions.
+
+Also, delete these ticks from all current users
+
+Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : ['hr', 'mail'],
+    'data':[
+        'pre_install.yml'
+        ],
+    'installable': True
+}
---
+++ res_users_hide_suggestions/models.py
@@ -0,0 +1,10 @@
+from openerp import api,models,fields
+
+class res_users(models.Model):
+    _inherit = 'res.users'
+
+    display_suggestions_hidden = fields.Boolean('Suggestions were hidden', default=False, help="Tick to don't repeat removing suggestion ticks")
+    _defaults = {
+        'display_groups_suggestions': False,
+        'display_employees_suggestions': False,
+    }
