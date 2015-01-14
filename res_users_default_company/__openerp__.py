72836d3dd06836baa395f5c7b7f06631e2cfbe74
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-14 19:26:18 +0200

    upload res_users_default_company

---
+++ res_users_default_company/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : "User's default company",
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+Adds field "default company". It can be used, for example, in Setting\Technical\Parameters\Company defaults.
+
+Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : [],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
---
+++ res_users_default_company/models.py
@@ -0,0 +1,6 @@
+from openerp import api,models,fields
+
+class res_users(models.Model):
+    _inherit = 'res.users'
+
+    company_default_id = fields.Many2one('res.company', string='Default company')
