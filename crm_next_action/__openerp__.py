4e0635a78700698dc3476c164085f156094e6809
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-20 19:38:03 +0200

    upload crm_next_action

---
+++ crm_next_action/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Log\Notification for Next Action',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+
+Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : ['crm'],
+    'data':['data.xml'],
+    'installable': True
+}
---
+++ crm_next_action/models.py
@@ -0,0 +1,17 @@
+from openerp.osv import osv, fields
+
+class crm_lead(osv.osv):
+    _inherit = 'crm.lead'
+
+    _track = {
+        'date_action': {
+            'crm_next_action.mt_lead_next_action_date': lambda self, cr, uid, obj, ctx=None: True
+        },
+        'title_action': {
+            'crm_next_action.mt_lead_next_action': lambda self, cr, uid, obj, ctx=None: True
+        }
+    }
+    _columns = {
+        'date_action': fields.date('Next Action Date', select=True, track_visibility='onchange'),
+        'title_action': fields.char('Next Action', track_visibility='onchange'),
+    }
