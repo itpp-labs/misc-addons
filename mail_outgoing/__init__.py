a0a2caa9cd696248f80c3f47608064de574d5dff
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-21 13:33:40 +0200

    upload mail_outgoing

---
+++ mail_outgoing/__init__.py
@@ -0,0 +1,1 @@
+import mail_outgoing_models
---
+++ mail_outgoing/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Outgoing mails menu',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+Allows to check outgoing mails, i.e. failed or delayed.
+
+Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : ['mail'],
+    'data':[
+        'security/mail_outgoing.xml',
+        'security/ir.model.access.csv',
+        'mail_outgoing_views.xml',
+        ],
+    'installable': True
+}
