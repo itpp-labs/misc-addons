9568e2eff9eec79c096d57ab838c431d59b1b034
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-26 20:18:23 +0200

    [IMP] upload res_partner_company_stat

---
+++ res_partner_company_stat/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Fix stat buttons for companies',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+Collects statistic information from all company's contacts.
+
+Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : ['crm', 'sale', 'account', 'project'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
