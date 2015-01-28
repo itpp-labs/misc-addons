ddfea9325c90451ae991ec3889c018c319a8f949
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-28 13:38:54 +0200

    [IMP] upload web_char_domain

---
+++ web_char_domain/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name' : 'Updated char_domain widget',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Sale',
+    'website' : 'https://it-projects.info',
+    'description': """
+Shows domain value at char_domain widget, e.g.
+
+    7 records selected -> Change selection : [('groups_id', '=', 3)]
+
+Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+    """,
+    'depends' : ['web'],
+    'data':[
+        'views.xml',
+        ],
+    'qweb' : [
+        "static/src/xml/*.xml",
+    ],
+    'installable': True
+}
