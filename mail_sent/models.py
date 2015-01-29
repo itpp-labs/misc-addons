70fa9e2c85442a53039be1879b77466d38044dc5
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-29 18:52:18 +0200

    [FIX] force update 'sent' field after calling _notify

--- mail_sent/models.py
+++ mail_sent/models.py
@@ -9,3 +9,10 @@ class mail_message(models.Model):
         self.sent = len(self.notified_partner_ids) > 1 or len(self.notified_partner_ids)==1 and self.notified_partner_ids[0].id != self.author_id.id
 
     sent = fields.Boolean('Sent', compute=_get_sent, help='Was message sent to someone', store=True)
+
+class mail_notification(models.Model):
+    _inherit = 'mail.notification'
+
+    def _notify(self, cr, uid, message_id, **kwargs):
+        super(mail_notification, self)._notify(cr, uid, message_id, **kwargs)
+        self.pool['mail.message'].browse(cr, uid, message_id)._get_sent()
