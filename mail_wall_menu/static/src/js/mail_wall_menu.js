944e97a1328743f7c96be80331fd810a4b7d1e74
Ivan Yelizariev <ivann.exe@gmail.com>
2015-01-30 20:15:53 +0200

    [FIX] floating blocks

--- mail_wall_menu/static/src/js/mail_wall_menu.js
+++ mail_wall_menu/static/src/js/mail_wall_menu.js
@@ -6,6 +6,24 @@ openerp.mail_wall_menu = function(instance){
         start: function(){
             if (this.action.params.disable_thread){
                 $('.oe_view_manager_body').addClass('dashboard_only');
+                var msnry = new Masonry( '.oe_mail_wall_aside', {
+                    // options
+                    columnWidth: 290,
+                    itemSelector: '.oe_goal',
+                    transitionDuration: 0
+                });
+                var update = -10;
+                setInterval(function(){
+                    update++;
+                    if (update > 0 && update % 5 != 0)
+                        return;
+                    if (update > 0)
+                        update = 0;
+                    msnry.reloadItems();
+                    msnry.layout();
+                }, 1000)
+
+
                 return;
             }
             this._super.apply(this, arguments)
