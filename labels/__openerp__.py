e44343cde2afde8cd47ab11ea627509020c06a8b
Ivan Yelizariev <ivann.exe@gmail.com>
2015-02-13 19:34:01 +0200

    [IMP] upload labels

---
+++ labels/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Labels',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Custom',
+    'website' : 'https://it-projects.info',
+    'description': """
+Adds templates for printing labels. See module labels_custom for example of usage.
+
+Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284
+    """,
+    'depends' : ['report'],
+    'data':[
+        'labels_views.xml',
+        'labels_templates.xml',
+        'labels_data.xml',
+        ],
+    'installable': True
+}
---
+++ labels_custom/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Labels (custom)',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Custom',
+    'website' : 'https://it-projects.info',
+    'description': """
+
+Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284
+    """,
+    'depends' : ['labels'],
+    'data':[
+        'views.xml',
+        'templates.xml',
+        ],
+    'installable': True
+}
