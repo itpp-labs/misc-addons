42025e4d707b56cba9ec4b4e40bed1b07cec6bd6
Ivan Yelizariev <ivann.exe@gmail.com>
2015-02-19 19:29:38 +0200

    add links to blog

--- im_notif/__openerp__.py
+++ im_notif/__openerp__.py
@@ -16,6 +16,8 @@ Options for notifications:
 * Only Emails
 
 Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
+
+Further information and discussion: https://yelizariev.github.io/odoo/module/2015/02/18/im-notifications.html
     """,
     'depends' : ['im_chat', 'mail'],
     'data':[
--- web_last_viewed_records/__openerp__.py
+++ web_last_viewed_records/__openerp__.py
@@ -12,6 +12,8 @@ This module doesn't affect on server performance, because it uses browser's loca
 FIXME: doesn't work in a res.config view
 
 Tested on 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d.
+
+Further information and discussion: https://yelizariev.github.io/odoo/module/2015/02/18/last-viewed-records.html
     """,
     'depends' : ['web', 'mail'],
     'data':[
