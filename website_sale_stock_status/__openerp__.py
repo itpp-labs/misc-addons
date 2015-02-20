93abcc59c701a7c840436e175dc0f23a3e38babe
Ivan Yelizariev <ivann.exe@gmail.com>
2015-02-20 11:39:56 +0200

    [IMP] link to blog

--- website_sale_stock_status/__openerp__.py
+++ website_sale_stock_status/__openerp__.py
@@ -17,6 +17,8 @@ Puts ribbons text at the product page.
 Disables "Add to cart" future if product is discontinued.
 
 Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284.
+
+Further information and discussion: http://yelizariev.github.io/odoo/module/2015/02/20/ecommerce-stock-status.html
     """,
     'depends' : ['website_sale', 'stock'],
     'data':[
