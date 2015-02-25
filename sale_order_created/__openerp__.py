48a97bc9946b2a0d20d730096710a2555ac4f55c
Ivan Yelizariev <ivann.exe@gmail.com>
2015-02-25 16:57:32 +0200

    upload sale_order_created

---
+++ sale_order_created/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Notification for new sale order (quotation)',
+    'version' : '1.0.0',
+    'author' : 'Ivan Yelizariev',
+    'category' : 'Custom',
+    'website' : 'https://yelizariev.github.io',
+    'description': """
+In the main, module is used to get notification about new quotations in ecommerce. To subscribe to new sale order (i.e. quotation), open related sales team (e.g. Website sales team) and add followers.
+
+Tested on Odoo 8.0 f8d5a6727d3e8d428d9bef93da7ba6b11f344284
+    """,
+    'depends' : ['sale'],
+    'data':[
+        'data.xml',
+        ],
+    'installable': True
+}
