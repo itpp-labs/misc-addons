83776cd0435c6c188465330599bf403534825859
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-13 09:17:35 +0200

    [IMP] add partner name to Repair Order's name
    [IMP] filter Repair order in phonecall form

--- phonecall_repair_order/models.py
+++ phonecall_repair_order/models.py
@@ -15,3 +15,13 @@ class mrp_repair(models.Model):
         self.phonecall_count = self.env['crm.phonecall'].search_count([('repair_id', '=', self.id)])
 
     phonecall_count = fields.Integer('Phonecalls Count', compute='_get_phonecall_count')
+
+    def name_get(self, cr, uid, ids, context=None):
+        #if not (context or {}).get('mrp_repair_extended_name'):
+        #    return super(mrp_repair, self).name_get(cr, uid, ids, context=context)
+
+        res = []
+        for r in self.browse(cr, uid, ids, context=context):
+            name = '%s [%s]' % (r.name, r.partner_id.display_name)
+            res.append((r.id, name))
+        return res
