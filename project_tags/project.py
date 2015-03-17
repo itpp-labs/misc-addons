8c8d9fca8921725167a70aad7181cbdb618205b3
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-17 12:13:47 +0200

    [ADD] project_tags

---
+++ project_tags/project.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Project Tags
+#    Copyright (C) 2013 Sistemas ADHOC
+#    No email
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+import re
+from openerp import netsvc
+from openerp.osv import osv, fields
+
+class project(osv.osv):
+    """"""
+    
+    _name = 'project.project'
+    _inherits = {  }
+    _inherit = [ 'project.project' ]
+
+    _columns = {
+        'project_tag_ids': fields.many2many('project_tags.project_tag', 'project_tags___project_tag_ids_rel', 'project_id', 'project_tag_id', string='Tags'), 
+    }
+
+    _defaults = {
+    }
+
+
+    _constraints = [
+    ]
+
+
+
+
+project()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ project_tags/project_tag.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Project Tags
+#    Copyright (C) 2013 Sistemas ADHOC
+#    No email
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+import re
+from openerp import netsvc
+from openerp.osv import osv, fields
+
+class project_tag(osv.osv):
+    """"""
+    
+    _name = 'project_tags.project_tag'
+    _description = 'project_tag'
+
+    _columns = {
+        'name': fields.char(string='Name', required=True, size=64),
+        'project_id': fields.many2many('project.project', 'project_tags___project_tag_ids_rel', 'project_tag_id', 'project_id', string='&lt;no label&gt;'), 
+    }
+
+    _defaults = {
+    }
+
+
+    _constraints = [
+    ]
+
+
+
+
+project_tag()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
