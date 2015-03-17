fe03644e99e3c375c59a3e3330b081fbc0b7507c
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-17 13:41:55 +0200

    [IMP] put country name in title attribute

--- project_kanban_customer/models.py
+++ project_kanban_customer/models.py
@@ -3,4 +3,5 @@ from openerp import api, models, fields, SUPERUSER_ID
 class project_project(models.Model):
     _inherit = 'project.project'
 
-    partner_country_image = fields.Binary('Partner\'s country', related='partner_id.country_id.image')
+    partner_country_image = fields.Binary('Partner\'s country flag', related='partner_id.country_id.image')
+    partner_country_name = fields.Char('Partner\'s country name', related='partner_id.country_id.name')
