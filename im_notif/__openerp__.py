33d2ccea4720417a4c859d45d51ea027fa9962ee
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-27 14:38:43 +0200

    [DOC] description for im_notif

--- im_notif/__openerp__.py
+++ im_notif/__openerp__.py
@@ -2,9 +2,10 @@
     'name' : 'IM Notifications',
     'version' : '1.0.0',
     'author' : 'Ivan Yelizariev',
-    'category' : 'Sale',
+    'category' : 'Tools',
     'website' : 'https://yelizariev.github.io',
     'depends' : ['im_chat', 'mail'],
+    'images': ['images/my-pref.png'],
     'data':[
         'im_notif_data.xml',
         'im_notif_views.xml',
