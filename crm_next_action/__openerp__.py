88c670a18667131961a5c230195860f38aade40c
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-28 14:53:31 +0200

    [DOC] description for crm_next_action

--- crm_next_action/__openerp__.py
+++ crm_next_action/__openerp__.py
@@ -1,13 +1,11 @@
 {
-    'name' : 'Log\Notification for Next Action',
+    'name' : 'Log\Notify Next Action changes',
     'version' : '1.0.0',
     'author' : 'Ivan Yelizariev',
     'category' : 'Sale',
     'website' : 'https://yelizariev.github.io',
-    'description': """
-
-Tested on Odoo 8.0 ab7b5d7732a7c222a0aea45bd173742acd47242d
-    """,
+    'price': 9.00,
+    'currency': 'EUR',
     'depends' : ['crm'],
     'data':['data.xml'],
     'installable': True
