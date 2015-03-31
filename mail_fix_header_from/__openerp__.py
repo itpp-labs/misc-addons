a21e0d49915b9285e105571049a1998699079027
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-31 16:05:49 +0200

    [DOC] mark module as obsolete for newest version

--- mail_fix_header_from/__openerp__.py
+++ mail_fix_header_from/__openerp__.py
@@ -1,10 +1,11 @@
 {
-    "name" : "Fix non-ascii header 'from'",
+    "name" : "Fix non-ascii header 'from' (OBSOLETE)",
     "version" : "0.3",
     "author" : "Ivan Yelizariev",
     "category" : "Mail",
     "website" : "https://yelizariev.github.io",
     "description": """
+Obsolete in odoo 8.0 since Sep 10, 2014 https://github.com/odoo/odoo/commit/f2cf6ced17d3477b8858e3a8f955a42cc8a629ff . You can install this module, if you use older version.
     """,
     "depends" : ["base"],
     #"init_xml" : [],
