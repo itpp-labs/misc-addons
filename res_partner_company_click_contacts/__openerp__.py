97ad1bf6637b24a087a4fc62475ca0f6a6756dba
Ivan Yelizariev <ivann.exe@gmail.com>
2015-04-13 14:57:38 +0200

    [ADD] res_partner_company_click_contacts

---
+++ res_partner_company_click_contacts/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Full view for company's contacts",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': [],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True
+}
