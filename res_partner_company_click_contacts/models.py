953ed361b9eb17cc76e67bccbf7cd1abe2573365
Ivan Yelizariev <ivann.exe@gmail.com>
2015-04-14 13:42:31 +0200

    replace <a type="open"> links to <a href="#id=1&model=res.partner">

--- res_partner_company_click_contacts/models.py
+++ res_partner_company_click_contacts/models.py
@@ -1,1 +1,10 @@
 from openerp import api, models, fields, SUPERUSER_ID
+
+class res_partner(models.Model):
+    _inherit = "res.partner"
+
+    record_url = fields.Char('Link to record', compute='compute_record_url')
+
+    @api.one
+    def compute_record_url(self):
+        self.record_url = '#id=%s&model=%s' % (self.id, self._name)
