e9c1d1dfb02089cc46bcd495c627a3ad1e54fc6f
Ivan Yelizariev <ivann.exe@gmail.com>
2015-04-14 12:42:01 +0200

    remove paid dependencies until better times

--- website_sales_team_custom/__openerp__.py
+++ website_sales_team_custom/__openerp__.py
@@ -11,8 +11,8 @@ Tested on Odoo 8.0 d023c079ed86468436f25da613bf486a4a17d625
     'depends' : [
         'res_users_clear_access_rights',
         'website_sales_team',
-        'website_sale_add_to_cart',
-        'website_sale_clear_cart',
+        #'website_sale_add_to_cart',
+        #'website_sale_clear_cart',
         'portal',
     ],
     'data':[
