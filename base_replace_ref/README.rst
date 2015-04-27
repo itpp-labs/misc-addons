1e9a07a274565c130fa0c2e24fe7229f7b0d1f6c
Ivan Yelizariev <ivann.exe@gmail.com>
2015-04-27 14:09:59 +0200

    [ADD] base_replace_ref

---
+++ base_replace_ref/README.rst
@@ -0,0 +1,6 @@
+Replace x2y references
+======================
+
+The module allows to create rules to execute replacement for relation field.
+
+Tested on Odoo 8.0 bf9544d7d430704efd006cca182a7120a55a0c8a
---
+++ base_replace_ref/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Replace x2y references",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': [],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True
+}
