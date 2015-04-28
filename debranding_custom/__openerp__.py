18e8f4ad31c3b077976b6cf6618f1516b330e7ef
Ivan Yelizariev <ivann.exe@gmail.com>
2015-04-28 15:11:42 +0200

    [ADD] debranding_custom

---
+++ debranding_custom/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Debranding (custom)",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['mail_delete_odoo_footer', 'disable_openerp_online', 'web_debranding', 'website_debranding'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True
+}
