dfb031f2b5f7e061b188430a8c3a9233ff7ee58e
Ilnur Gallyamov <gallyamov@it-projects.info>
2015-05-13 14:16:22 +0500

    simple code

--- res_partner_strip_email/models.py
+++ res_partner_strip_email/models.py
@@ -17,7 +17,6 @@ class res_partner_strip_email(models.Model):
         return super(res_partner_strip_email, self).create(vals)
 
     def _check_email_field(self, vals):
-        if not isinstance(vals['email'], bool) and 'email' in vals or \
-                isinstance(vals['email'], bool) and vals['email']:
+        if vals.get('email'):
             vals['email'] = vals['email'].strip()
         return vals
