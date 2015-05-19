6e676ba3c3bc18d9ea1a0ad7295f3430462a1f1e
Ivan Yelizariev <ivann.exe@gmail.com>
2015-05-19 18:24:14 +0200

    upload reminder_task_deadline

---
+++ reminder_task_deadline/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Reminders for tasks",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['reminder_base', 'project'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True,
+}
---
+++ reminder_task_deadline/models.py
@@ -0,0 +1,8 @@
+from openerp import models
+
+
+class task(models.Model):
+    _name = 'project.task'
+    _inherit = ['project.task', 'reminder']
+
+    _reminder_date_field = 'date_deadline'
