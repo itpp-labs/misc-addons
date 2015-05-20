cd6098df05f4b1a01f3bce0c7a3dd7d2524b6746
Ivan Yelizariev <ivann.exe@gmail.com>
2015-05-20 17:45:36 +0200

    [DOC] description fro reminder_phonecall

--- reminder_phonecall/README.rst
+++ reminder_phonecall/README.rst
@@ -1,4 +1,6 @@
 Reminders and Agenda for phonecalls
 ===================================
 
+Description: https://apps.odoo.com/apps/modules/8.0/reminder_phonecall/
+
 Tested on Odoo 8.0 ea60fed97af1c139e4647890bf8f68224ea1665b
--- reminder_phonecall/__openerp__.py
+++ reminder_phonecall/__openerp__.py
@@ -1,9 +1,11 @@
 {
-    'name': "Reminders and Agenda for phonecalls",
+    'name': "Reminders and Agenda for Phonecalls",
     'version': '1.0.0',
     'author': 'Ivan Yelizariev',
     'category': 'Custom',
     'website': 'https://yelizariev.github.io',
+    'price': 6.00,
+    'currency': 'EUR',
     'depends': ['reminder_base', 'crm'],
     'data': [
         'views.xml',
