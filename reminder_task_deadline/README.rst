e5251294390f98a0d979c74624c705f013100d09
Ivan Yelizariev <ivann.exe@gmail.com>
2015-05-20 17:46:40 +0200

    [DOC] description for reminder_task_deadline

--- reminder_task_deadline/README.rst
+++ reminder_task_deadline/README.rst
@@ -1,4 +1,6 @@
 Reminders and Agenda for tasks
 ==============================
 
+Description: https://apps.odoo.com/apps/modules/8.0/reminder_task_deadline/
+
 Tested on Odoo 8.0 ea60fed97af1c139e4647890bf8f68224ea1665b
--- reminder_task_deadline/__openerp__.py
+++ reminder_task_deadline/__openerp__.py
@@ -1,9 +1,11 @@
 {
-    'name': "Reminders and Agenda for tasks",
+    'name': "Reminders and Agenda for Tasks",
     'version': '1.0.0',
     'author': 'Ivan Yelizariev',
     'category': 'Custom',
     'website': 'https://yelizariev.github.io',
+    'price': 6.00,
+    'currency': 'EUR',
     'depends': ['reminder_base', 'project'],
     'data': [
         'views.xml',
