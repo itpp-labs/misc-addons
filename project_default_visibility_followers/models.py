8a07bbc259cee479ea6397e1fcaf3bce83c46ec0
Ilnur Gallyamov <gallyamov@it-projects.info>
2015-05-22 21:30:33 +0500

    v 1.0.0

---
+++ project_default_visibility_followers/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+
+class project_default_visibility_followers(models.Model):
+    _inherit = 'project.project'
+    _defaults = {'privacy_visibility':  'followers'}
