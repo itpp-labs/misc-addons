549a6a0f9bdc7135bc8b2e6fb2483f421b7b8b71
s0x90 <mail@102web.ru>
2015-06-19 11:26:55 +0500

    !!!

--- website_sale_birthdate/controllers.py
+++ website_sale_birthdate/controllers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 
+from openerp.http import request
 import openerp.addons.website_sale.controllers.main as main
 
 
@@ -9,6 +10,13 @@ class WebsiteSaleBirthdate(main.website_sale):
     def checkout_values(self, data=None):
 
         values = super(WebsiteSaleBirthdate, self).checkout_values(data=None)
+
+        current_user = request.env.user
+        print '##################'
+        print current_user.id
+        print request.website.user_id.id
+        orm_partner = request.env['res.partner']
+        print orm_partner.browse(request.website.user_id.id).birthdate
         values['checkout']['birthdate'] = datetime.date.today()
 
         return values
