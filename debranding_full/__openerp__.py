6c7dd2540f282a2ac737a8f917cdbbef8f49f886
Ivan Yelizariev <ivann.exe@gmail.com>
2015-06-22 09:23:06 +0500

    [ADD] debranding_full module

---
+++ debranding_full/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Full Debranding",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['mail_delete_sent_by_footer', 'disable_openerp_online', 'web_debranding', 'website_debranding'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True
+}
---
+++ debranding_full/models.py
@@ -0,0 +1,1 @@
+from openerp import api, models, fields, SUPERUSER_ID
