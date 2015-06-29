6dede732dae0da59039ded69589a5ec60f5272d8
Ivan Yelizariev <yelizariev@it-projects.info>
2015-06-29 13:14:41 +0500

    [FIX] system would fails if user delete web_debranding.new_name parameter [IMP] translate "Software" -- default value for debranding

--- web_debranding/models.py
+++ web_debranding/models.py
@@ -1,5 +1,5 @@
 from openerp import api, models, fields, SUPERUSER_ID
-
+from openerp.tools.translate import _
 
 class ir_actions_act_window_debranding(models.Model):
     _inherit = 'ir.actions.act_window'
@@ -9,9 +9,9 @@ class ir_actions_act_window_debranding(models.Model):
         results = super(ir_actions_act_window_debranding, self).read(
             cr, uid, ids, fields=fields, context=context, load=load)
         if not fields or 'help' in fields:
-            tmp_name = self.pool.get('ir.config_parameter').get_param(
-                cr, uid, 'web_debranding.new_name', False, context).strip()
-            new_name = tmp_name == '' and 'Software' or tmp_name
+            new_name = self.pool.get('ir.config_parameter').get_param(
+                cr, uid, 'web_debranding.new_name', False, context)
+            new_name = new_name and new_name.strip() or  _('Software')
             for res in results:
                 if type(res) is dict and res.get('help'):
                     res['help'] = res['help'].replace('Odoo', new_name)
