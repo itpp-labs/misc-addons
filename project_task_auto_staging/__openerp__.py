5995997615527a85a650240a1bea82e82c4d393e
Ilnur Gallyamov <gallyamov@it-projects.info>
2015-06-30 10:20:53 +0500

    fix notice

--- project_task_auto_staging/__openerp__.py
+++ project_task_auto_staging/__openerp__.py
@@ -7,7 +7,6 @@
     'version': '1.0.0',
     'depends': ['project'],
     'data': [
-        'js.xml',
         'views.xml',
         'data.xml',
     ],
--- project_task_auto_staging/models.py
+++ project_task_auto_staging/models.py
@@ -15,8 +15,15 @@ class project_task_type_auto_staging(models.Model):
     _inherit = 'project.task.type'
 
     to_stage_automove_id = fields.Many2one('project.task.type')
-    delay_automove = fields.Integer(default=12)
+    delay_automove = fields.Integer()
     active_move = fields.Boolean('Enable auto move', default=False)
+
+    @api.onchange('active_move')
+    def _onchange_active_move(self):
+        if not self.active_move:
+            self.delay_automove = 0
+        else:
+            self.delay_automove = 12
 
     @api.one
     @api.constrains('delay_automove')
@@ -24,24 +31,21 @@ class project_task_type_auto_staging(models.Model):
         if self.active_move and self.delay_automove <= 0:
             raise ValidationError(
                 "Value of 'Delay' field  must be greater than 0")
-        elif not self.active_move:
-            if self.delay_automove < 0:
-                self.delay_automove = -self.delay_automove
-            elif self.delay_automove == 0:
-                self.delay_automove = 12
 
 
 class project_task_auto_staging(models.Model):
     _inherit = 'project.task'
     allow_automove = fields.Boolean(compute='_get_allow_automove')
     delay_automove = fields.Integer(
-        string='Delay', related='stage_id.delay_automove',
-        track_visibility='always', readonly=True)
+        string='Delay', related='stage_id.delay_automove', readonly=True)
     automove_to_stage_id = fields.Many2one(
         'project.task.type', readonly=True,
         related='stage_id.to_stage_automove_id')
     when_date_automove = fields.Date(
         string='When', compute='_get_when_date_automove')
+    days_to_automove = fields.Integer(
+        string='Days to automove:', compute='_get_days_to_automove',
+        track_visibility='always')
     _track = {
         'stage_id': {
             'project_task_auto_staging.mt_auto_move':
@@ -62,26 +66,20 @@ class project_task_auto_staging(models.Model):
         self.when_date_automove = datetime.datetime.strptime(
             self.write_date, DEFAULT_SERVER_DATETIME_FORMAT) + delta
 
-    @api.model
-    def get_rest_day(self, write_date):
+    @api.one
+    def _get_days_to_automove(self):
         today = datetime.datetime.now()
-        last_date = datetime.datetime.strptime(
-            write_date, DEFAULT_SERVER_DATETIME_FORMAT)
-        delta = today - last_date
-        return delta.days
-
-    @api.model
-    def get_today(self):
-        return fields.Datetime.now()
+        date_modifications = datetime.datetime.strptime(
+            self.write_date, DEFAULT_SERVER_DATETIME_FORMAT)
+        delta = today - date_modifications
+        self.days_to_automove = self.delay_automove - delta.days
 
     @api.model
     def _cron_move_tasks(self):
         today = fields.Date.context_today(self)
         pt_env = self.env['project.task']
         tasks = pt_env.search([])
-        print "IR_CRON: project_task_auto_staging start"
         for task in tasks:
             if task.allow_automove and today == task.when_date_automove:
                 task.with_context(auto_staging=True).write(
                     {'stage_id':  task.automove_to_stage_id.id})
-        print "IR_CRON: project_task_auto_staging done"
