f5ffae8fb6dc4c5ff2d3e63268b87a59fa9b15be
Ivan Yelizariev <yelizariev@it-projects.info>
2015-07-08 09:40:06 +0500

    [DOC] add description to mail_sent

--- mail_sent/__openerp__.py
+++ mail_sent/__openerp__.py
@@ -1,9 +1,11 @@
 {
-    'name' : 'Sent mails menu',
+    'name' : 'Sentbox',
     'version' : '1.0.0',
     'author' : 'Ivan Yelizariev',
     'category' : 'Social Network',
     'website' : 'https://yelizariev.github.io',
+    'price': 9.00,
+    'currency': 'EUR',
     'depends' : ['mail'],
     'data':[
         'views.xml',
