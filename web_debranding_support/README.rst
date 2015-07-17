129901fdbca6233e5a1efc315578eea0f1e21597
Ivan Yelizariev <yelizariev@it-projects.info>
2015-07-17 17:39:04 +0500

    [ADD] web_debranding_support

---
+++ web_debranding_support/README.rst
@@ -0,0 +1,9 @@
+Backend debranding + Support branding
+=====================================
+
+Fixes compatibility between two modules:
+
+* web_debranding
+* support_branding
+
+Tested on Odoo 8.0 75f0c7df4dc016b5e0ace4db5b6487fc5a21632a
---
+++ web_debranding_support/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Backend debranding + Support branding",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Debranding',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['support_branding', 'web_debranding'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True,
+    'auto_install': True,
+}
