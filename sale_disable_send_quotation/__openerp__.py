92cd6b8e9dd713643b576ec6d9a4a3b4afc3f3e3
Ivan Yelizariev <yelizariev@it-projects.info>
2015-08-08 10:44:46 +0500

    [ADD] upload sale_disable_send_quotation

---
+++ sale_disable_send_quotation/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Disable sending quotations on sales",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Sales Management',
+    'website': 'https://yelizariev.github.io',
+    'price': 9.00,
+    'currency': 'EUR',
+    'depends': ['sale'],
+    'images': ['images/form.png'],
+    'data': [
+        'views.xml',
+        ],
+    'installable': True,
+}
