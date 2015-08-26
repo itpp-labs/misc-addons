d26109c13685e329258a2e0e8be241fa5e59a845
ufaks <excellm@yandex.ru>
2015-08-26 21:15:59 +0500

    [IMP] add track changes for fleet odometer

---
+++ fleet_odometer_track_changes/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fleet with odometer track changes',
+    'version': '0.1',
+    'author': 'no name',
+    'category': 'Managing vehicles and contracts',
+    'website': 'https://www.odoo.com',
+    'summary': 'Vehicle, leasing, insurances, costs',
+    'description': """
+
+    """,
+    'depends': ['fleet'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+}
