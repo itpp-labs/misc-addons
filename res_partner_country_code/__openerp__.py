888dd3d4604272996bc020eb0029c7f4f95c1f4e
ufaks <excellm@yandex.ru>
2015-08-31 12:41:28 +0500

    unknown JS error

---
+++ res_partner_country_code/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Country selection by email address top-lever domain",
+    'version': '1.0',
+    'author': "Ivan Yelizariev",
+    'website': "https://yelizariev.github.io",
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': ['views.xml'],
+    'demo': [],
+    'installable': True,
+}
