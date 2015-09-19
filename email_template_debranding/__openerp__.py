20e904103d13d17dec9a914f35a434b22ed30b56
Salton <smassally@idtlabs.sl>
2015-09-19 21:37:39 +0000

    [ADD] email_template_debranding Debrands email template e.g. the password reset template

---
+++ email_template_debranding/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Email Template debranding",
+    'version': '1.0.0',
+    'author': 'Salton Massally',
+    'category': 'Debranding',
+    'website': 'http://idtlabs.sl',
+    'depends': ['email_template', 'web_debranding'],
+    'auto_install': True,
+    'installable': True
+}
