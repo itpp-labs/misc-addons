276988a9ddd25e7cd958b780f73a7b6990aacdd1
iledarn <iledarn@gmail.com>
2015-09-23 15:18:51 +0500

    [ADD] reminder_hr_recruitment module

---
+++ reminder_hr_recruitment/models.py
@@ -0,0 +1,10 @@
+from openerp import api, models, fields, SUPERUSER_ID
+
+
+class hr_applicant(models.Model):
+    _name = 'hr.applicant'
+    _inherit = ['hr.applicant', 'reminder']
+    _reminder_date_field = 'date_action'
+    _reminder_description_field = 'title_action'
+    reminder_alarm_ids = fields.Many2many(string='Next Action Reminders')
+
