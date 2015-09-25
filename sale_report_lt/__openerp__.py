0960302d6bcb023e3418b4cbf26ccf09c3828421
Cesar Lage <kaerdsar@gmail.com>
2015-09-25 15:00:27 -0400

    [IMP] add minor modifications for invoice report in Lithuania

---
+++ sale_report_lt/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    "name" : "Sale Report - Lithuania",
+    "version" : "0.1",
+    "author" : "Cesar Lage",
+    "category" : "Accounting",
+    "website" : "https://yelizariev.github.io",
+    "description": """
+        Sale Report - Lithuania
+    """,
+    "depends": ["account"],
+    "data": ['report_invoice.xml',],
+    "installable": True
+}
