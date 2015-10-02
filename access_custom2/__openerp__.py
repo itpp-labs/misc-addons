9f21d2187cd3ead674315d466a474c863e2fa381
Cesar Lage <kaerdsar@gmail.com>
2015-10-02 12:22:11 -0400

    add custom groups, include a new module to hide specific groups in menu

---
+++ access_custom2/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom security groups 2',
+    'version': '1.0.0',
+    'author': 'Cesar Lage',
+    'category': 'Tools',
+    'website': 'https://yelizariev.github.io',
+    'description': """
+            Custom security groups 2
+    """,
+    'depends': ['access_base', 'sale', 'crm', 'stock', 'point_of_sale', 'group_menu_no_access'],
+    'data': [
+        'views.xml',
+        'security.xml',
+        #'ir.model.access.csv',
+        ],
+    'installable': True
+}
