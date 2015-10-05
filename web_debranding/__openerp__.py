288824812ca50a1db5a58a2922c3160de109ecbd
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-05 12:38:28 +0500

    [PORT] elete share module from dependencies

--- web_debranding/__openerp__.py
+++ web_debranding/__openerp__.py
@@ -6,7 +6,7 @@
     'website': 'https://yelizariev.github.io',
     'price': 81.00,
     'currency': 'EUR',
-    'depends': ['web', 'share', 'disable_openerp_online', 'mail_delete_sent_by_footer'],
+    'depends': ['web', 'disable_openerp_online', 'mail_delete_sent_by_footer'],
     'data': [
         'security/web_debranding_security.xml',
         'data.xml',
