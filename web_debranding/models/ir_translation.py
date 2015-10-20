d4c6ca50f22cbbb4d49a34c0016720ff5fe7e189
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-20 12:40:53 +0500

    [PORT] fix: debrand via translation tool

--- web_debranding/models/ir_translation.py
+++ web_debranding/models/ir_translation.py
@@ -9,6 +9,11 @@ from openerp import SUPERUSER_ID, models, tools, api
 class ir_translation(models.Model):
     _inherit = 'ir.translation'
 
+    def _debrand_dict(self, res):
+        for k in res:
+            res[k] = self._debrand(res[k])
+        return res
+
     def _debrand(self, cr, uid, source):
         if not source or not re.search(r'\bodoo\b', source, re.IGNORECASE):
             return source
@@ -20,7 +25,19 @@ class ir_translation(models.Model):
 
         return re.sub(r'\bodoo\b', new_name, source, flags=re.IGNORECASE)
 
-    @tools.ormcache(skiparg=3)
-    def _get_source(self, cr, uid, name, types, lang, source=None, res_id=None):
-        res = super(ir_translation, self)._get_source(cr, uid, name, types, lang, source, res_id)
+    @tools.ormcache('name', 'types', 'lang', 'source', 'res_id')
+    def __get_source(self, cr, uid, name, types, lang, source, res_id):
+        res = super(ir_translation, self).__get_source(cr, uid, name, types, lang, source, res_id)
         return self._debrand(cr, uid, res)
+
+    @api.model
+    @tools.ormcache_context('model_name', keys=('lang',))
+    def get_field_string(self, model_name):
+        res = super(ir_translation, self).get_field_string(model_name)
+        return self._debrand_dict(res)
+
+    @api.model
+    @tools.ormcache_context('model_name', keys=('lang',))
+    def get_field_help(self, model_name):
+        res = super(ir_translation, self).get_field_help(model_name)
+        return self._debrand_dict(res)
