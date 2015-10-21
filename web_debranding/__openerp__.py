f8254ea1558f43dc651b80f26a297f71bcf1659a
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-21 12:32:11 +0500

    [DOC] set the same price for 8.0 as for 9.0

--- web_debranding/__openerp__.py
+++ web_debranding/__openerp__.py
@@ -4,7 +4,7 @@
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'category': 'Debranding',
     'website': 'https://yelizariev.github.io',
-    'price': 81.00,
+    'price': 90.00,
     'currency': 'EUR',
     'depends': ['web', 'share', 'disable_openerp_online', 'mail_delete_sent_by_footer'],
     'data': [
