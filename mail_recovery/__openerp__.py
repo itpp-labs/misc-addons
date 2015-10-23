42bbcee83627d488bc6f556e261fc5f43a374784
Ildar Nasyrov <nasyrov@it-projects.info>
2015-10-23 15:51:35 +0500

    [REF] module version

--- mail_recovery/__openerp__.py
+++ mail_recovery/__openerp__.py
@@ -4,7 +4,7 @@
     'author': "IT-Projects LLC, Ildar Nasyrov",
     'website': "https://twitter.com/nasyrov_ildar",
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0.0',
     'depends': ['mail'],
     'data': [
         'data.xml',
