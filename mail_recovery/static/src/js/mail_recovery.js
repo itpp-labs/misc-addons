ad51d015f858fa1c81839216d0d74ca2b9f7466e
Ildar Nasyrov <nasyrov@it-projects.info>
2015-10-24 12:29:18 +0500

    [FIX] keypress to keyup

--- mail_recovery/static/src/js/mail_recovery.js
+++ mail_recovery/static/src/js/mail_recovery.js
@@ -5,7 +5,7 @@ openerp.mail_recovery = function (session) {
         bind_events: function () {
             var self = this;
             this.$('textarea').on('focus', self.on_focus_textarea);
-            this.$('textarea').on('keypress', self.on_keypress_textarea);
+            this.$('textarea').on('keyup', self.on_keyup_textarea);
             this._super();
         },
         on_focus_textarea: function(event) {
@@ -14,7 +14,7 @@ openerp.mail_recovery = function (session) {
                 $input.val(window.localStorage['message_storage']);
             }
         },
-        on_keypress_textarea: function(event) {
+        on_keyup_textarea: function(event) {
             window.localStorage['message_storage'] = $(event.target).val();
         },
         on_message_post: function (event) {
