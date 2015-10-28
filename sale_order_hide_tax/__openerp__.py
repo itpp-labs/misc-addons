48b0db90bc5c1830ec4f7fc35ca5eee1baaaf66c
Ildar Nasyrov <nasyrov@it-projects.info>
2015-10-28 12:42:21 +0500

    [REF] description

--- sale_order_hide_tax/__openerp__.py
+++ sale_order_hide_tax/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Sale order without taxes information",
+    'name': "Sale order with tax information hided",
     'author': "IT-Projects LLC, Ildar Nasyrov",
     'website': "https://twitter.com/nasyrov_ildar",
     'category': 'Uncategorized',
