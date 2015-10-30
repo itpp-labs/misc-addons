ea2cb28ac7fa679983548aab2823f000e764680d
Ildar Nasyrov <nasyrov@it-projects.info>
2015-10-30 15:13:45 +0500

    [DOC] help

--- sale_contract_slots/models.py
+++ sale_contract_slots/models.py
@@ -5,8 +5,9 @@ from openerp import models, fields, api
 class ProductTemplateSlots(models.Model):
     _inherit = 'product.template'
 
-    slots = fields.Integer(default=0, help="""Type -1 if you want to indicate one usage of bulk service by this product.\n
-    Type number of slots if you want to indicate selling your service in bulk by this product. Leave zero otherwise""")
+    slots = fields.Integer(default=0, help="""Slot number to sale in bulk.
+    Can be negative to account slots usage (usually used for zero priced dummy products).
+    Leave zero if you don't use slots.""")
 
 
 class AccountAnalyticAccountSlots(models.Model):
