484fb1aae7ff2e9ea3fb3dd7969d15bdc8cf49f0
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-30 20:31:48 +0500

    [FIX] we cannot restrict read access it brokes security rules (e.g. system consider menus without groups and make it accessable)

--- access_restricted/models.py
+++ access_restricted/models.py
@@ -10,15 +10,19 @@ class ResUsers(models.Model):
         if view_type == 'form':
             last_uid = self.pool['ir.config_parameter'].get_param(cr, uid, IR_CONFIG_NAME, context=context)
             if int(last_uid) != uid:
-                self.pool['res.groups'].update_user_groups_view(cr, uid, context=context)
+                ctx = (context or {}).copy()
+                ctx['access_restricted'] = 1
+                self.pool['res.groups'].update_user_groups_view(cr, uid, context=ctx)
 
         return super(ResUsers, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar, submenu)
 
     def fields_get(self, cr, uid, allfields=None, context=None, write_access=True, attributes=None):
-        # inherited as uid here is always SUPERUSER_ID
+        # uid is SUPERUSER_ID, so we need to change it
         uid = self.pool['ir.config_parameter'].get_param(cr, uid, IR_CONFIG_NAME, context=context)
         uid = int(uid)
-        return super(ResUsers, self).fields_get(cr, uid, allfields=None, context=None, write_access=True, attributes=None)
+        ctx = (context or {}).copy()
+        ctx['access_restricted'] = 1
+        return super(ResUsers, self).fields_get(cr, uid, allfields=allfields, context=ctx, write_access=write_access, attributes=attributes)
 
 
 class ResGroups(models.Model):
@@ -26,4 +30,16 @@ class ResGroups(models.Model):
 
     def update_user_groups_view(self, cr, uid, context=None):
         self.pool['ir.config_parameter'].set_param(cr, uid, IR_CONFIG_NAME, uid, context=context)
-        return super(ResGroups, self).update_user_groups_view(cr, uid, context=context)
+        ctx = (context or {}).copy()
+        ctx['access_restricted'] = 1
+        return super(ResGroups, self).update_user_groups_view(cr, uid, context=ctx)
+
+    def get_application_groups(self, cr, uid, domain=None, context=None):
+        if domain is None:
+            domain = []
+        domain.append(('share', '=', False))
+        if uid != SUPERUSER_ID and (context or {}).get('access_restricted'):
+            model_data_obj = self.pool.get('ir.model.data')
+            _model, group_no_one_id = model_data_obj.get_object_reference(cr, uid, 'base', 'group_no_one')
+            domain = domain + ['|', ('users', 'in', [uid]), ('id', '=', group_no_one_id)]
+        return self.search(cr, uid, domain, context=context)
