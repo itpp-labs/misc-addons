b389ab9a82c16aed91291f36767926b658fff1a1
Ivan Yelizariev <yelizariev@it-projects.info>
2015-10-30 16:17:14 +0500

    [ADD] access_restricted

---
+++ access_restricted/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Restricted administration rights',
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'category': 'Tools',
+    'website': 'https://twitter.com/yelizariev',
+    'depends': [],
+    'data': [
+        'security.xml',
+    ],
+    'installable': True
+}
---
+++ access_restricted/models.py
@@ -0,0 +1,22 @@
+from openerp import models, fields, api
+
+IR_CONFIG_NAME = 'access_restricted.fields_view_get_uid'
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        if view_type == 'form':
+            last_uid = self.pool['ir.config_parameter'].get_param(cr, uid, IR_CONFIG_NAME, context=context)
+            if int(last_uid) != uid:
+                self.pool['ir.config_parameter'].set_param(cr, uid, IR_CONFIG_NAME, uid, context=context)
+                self.pool['res.groups'].update_user_groups_view(cr, uid, context=context)
+
+        return super(ResUsers, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar, submenu)
+
+    def fields_get(self, cr, uid, allfields=None, context=None, write_access=True, attributes=None):
+        # inherited as uid here is always SUPERUSER_ID
+        uid = self.pool['ir.config_parameter'].get_param(cr, uid, IR_CONFIG_NAME, context=context)
+        uid = int(uid)
+        return super(ResUsers, self).fields_get(cr, uid, allfields=None, context=None, write_access=True, attributes=None)
