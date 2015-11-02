91307f070a871ce2e7369462ee4ab4fdb24bdbf0
Ildar Nasyrov <nasyrov@it-projects.info>
2015-11-02 15:02:33 +0500

    [FIX] sum slots in accordance with product qty

--- sale_contract_slots/models.py
+++ sale_contract_slots/models.py
@@ -15,14 +15,14 @@ class AccountAnalyticAccountSlots(models.Model):
 
     available_slots = fields.Integer(compute='_compute_available_slots', readonly=True, help='remaining number of slots left in this contract')
 
-    @api.one
+    @api.model
     def _compute_available_slots(self):
         lines = self.env['sale.order.line'].search(['&', '&', ('order_id.project_id', '=', self.id),
                                                     ('order_id.state', 'in', ['manual', 'done']),
                                                     '|',
                                                     ('price_unit', '=', '0'),
                                                     ('product_id.slots', '!=', '0')])
-        self.available_slots = sum(lines.mapped(lambda r: r.product_id.slots))
+        self.available_slots = sum(lines.mapped(lambda r: r.product_id.slots * r.product_uom_qty))
 
 
 class SaleOrderSlots(models.Model):
