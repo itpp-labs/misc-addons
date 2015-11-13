54120ddb2c143c2dd2e20e6740d77ec340802ff1
Ildar Nasyrov <nasyrov@it-projects.info>
2015-11-13 10:40:34 +0500

    [ADD] booking_calendar_analytic module

---
+++ booking_calendar_analytic/README.rst
@@ -0,0 +1,8 @@
+Booking Calendar Analytic
+=========================
+
+This module provides an opportunity for grouping booking lines by contract. Additional button "Booking List"
+will appear in Contract form, when you install this module.
+
+
+Tested on Odoo 8.0 e84c01ebc1ef4fdf99865c45f10d7b6b4c4de229
---
+++ booking_calendar_analytic/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Booking Calendar Analytic",
+    'author': "IT-Projects LLC, Ildar Nasyrov",
+    'license': 'GPL-3',
+    'website': "https://twitter.com/nasyrov_ildar",
+    'category': 'Sale',
+    'version': '1.0.0',
+    'depends': ['booking_calendar', 'sale_contract_slots', 'pitch_booking'],
+    'data': [
+        'views.xml',
+    ],
+}
