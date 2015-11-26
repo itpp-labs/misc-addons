caf7a4d60b001be7a5e2ee55820a676864ab5829
Ildar Nasyrov <nasyrov@it-projects.info>
2015-11-26 16:31:56 +0500

    [ADD] automatically generate booking lines

--- thecage_data/models.py
+++ thecage_data/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api
-from datetime import datetime, timedelta
+from datetime import datetime, date, timedelta
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 
@@ -42,6 +42,12 @@ class SaleOrderTheCage(models.Model):
                 self.env['sms_sg.sendandlog'].send_sms(phone, msg)
         return result
 
+    @api.multi
+    def remove_generated_lines(self):
+        records = self.env['sale.order.line'].search([('order_id', '=', self[0].id), ('automatic', '=', True)])
+        records.button_cancel()
+        records.unlink()
+
 
 class SaleOrderLineReminder(models.Model):
     _inherit = 'sale.order.line'
@@ -69,3 +75,61 @@ class ResPartnerReminderConfig(models.Model):
 
     reminder_sms = fields.Boolean(default=True, string='Booking sms reminder enabled')
     reminder_email = fields.Boolean(default=True, string='Booking email reminder enabled')
+
+
+class GenerateBookingWizard(models.TransientModel):
+    _name = 'thecage_data.generate_booking_wizard'
+
+    quantity = fields.Integer(string='Number of bookings to generate', default=52)
+    product_id = fields.Many2one('product.product', string='Product')
+    venue_id = fields.Many2one('pitch_booking.venue', string='Venue', related='product_id.venue_id')
+    pitch_id = fields.Many2one('pitch_booking.pitch', string='Pitch')
+    booking_start = fields.Datetime(string='Booking start')
+    booking_end = fields.Datetime(string='Booking end')
+
+    day_of_week = fields.Selection([(0, 'Monday'),
+                                    (1, 'Tuesday'),
+                                    (2, 'Wednesday'),
+                                    (3, 'Thursday'),
+                                    (4, 'Friday'),
+                                    (5, 'Saturday'),
+                                    (6, 'Sunday')],
+                                   compute='_compute_day_of_week', readonly=True)
+
+    def default_get(self, cr, uid, fields, context=None):
+        result = super(GenerateBookingWizard, self).default_get(cr, uid, fields, context=context)
+        active_id = context and context.get('active_id', False)
+        active_order = self.pool['sale.order'].browse(cr, uid, active_id, context=context)
+        result.update({
+            'product_id': active_order.order_line[0].product_id.id,
+            'pitch_id': active_order.order_line[0].pitch_id.id,
+            'booking_start': active_order.order_line[0].booking_start,
+            'booking_end': active_order.order_line[0].booking_end
+        })
+        return result
+
+    @api.one
+    @api.depends('booking_start')
+    def _compute_day_of_week(self):
+        dt = datetime.strptime(self.booking_start, DEFAULT_SERVER_DATETIME_FORMAT)
+        self.day_of_week = date(dt.year, dt.month, dt.day).weekday()
+
+    @api.multi
+    def generate_booking_lines(self):
+        active_id = self.env.context and self.env.context.get('active_id', False)
+        active_order = self.env['sale.order'].browse(active_id)
+        booking_start = datetime.strptime(self.booking_start, DEFAULT_SERVER_DATETIME_FORMAT)
+        booking_end = datetime.strptime(self.booking_end, DEFAULT_SERVER_DATETIME_FORMAT)
+
+        for line in range(0, self[0].quantity):
+            booking_start = booking_start + timedelta(days=7)
+            booking_end = booking_end + timedelta(days=7)
+
+            self.env['sale.order.line'].create({'order_id': active_order.id,
+                                                'product_id': self[0].product_id.id,
+                                                'venue_id': self[0].venue_id.id,
+                                                'pitch_id': self[0].pitch_id.id,
+                                                'booking_start': booking_start,
+                                                'booking_end': booking_end,
+                                                'automatic': True,
+                                                'state': 'draft'})
