802e894ba9817a640893536f4fdf8e40d154d436
Ildar Nasyrov <nasyrov@it-projects.info>
2015-11-26 16:43:18 +0500

    [FIX] sms reminder

--- thecage_data/models.py
+++ thecage_data/models.py
@@ -35,8 +35,8 @@ class SaleOrderTheCage(models.Model):
     @api.one
     def write(self, vals):
         result = super(SaleOrderTheCage, self).write(vals)
-        if vals.get('state') == 'sent':  # send sms immediately after user pushed 'Send by Email' button on the Sale Order
-            if self.partner_id.reminder_sms:
+        # send sms immediately after user pushed 'Send by Email' button on the Sale Order
+        if vals.get('state') == 'sent' and self.partner_id.reminder_sms:
                 msg = 'Sale Order #' + self.name + ' is confirmed'
                 phone = self.partner_id.mobile
                 self.env['sms_sg.sendandlog'].send_sms(phone, msg)
