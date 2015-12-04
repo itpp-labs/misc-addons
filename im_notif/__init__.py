90abd29fc8ba7edbaf35f9bfc5aabfeec4699a08
app <github@andreparames.com>
2015-12-04 18:10:05 +0000

    [ADD] Fix partner notify_email before uninstall

--- im_notif/__init__.py
+++ im_notif/__init__.py
@@ -1,1 +1,8 @@
 import im_notif_models
+
+
+def pre_uninstall(cr, registry):
+    query = ("UPDATE res_partner "
+             "SET notify_email = 'always' "
+             "WHERE notify_email LIKE 'im%';")
+    cr.execute(query)
