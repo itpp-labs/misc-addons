ba20372ff9d11e620a5ed1e7089ba44754ca6cf2
Ildar Nasyrov <nasyrov@it-projects.info>
2016-01-04 10:25:40 +0500

    [FIX] dashboards in web_debranding

--- web_debranding/static/src/js/dashboard.js
+++ web_debranding/static/src/js/dashboard.js
@@ -4,7 +4,11 @@ odoo.define('web_debranding.dashboard', function (require) {
     var dashboard = require('web_settings_dashboard');
     dashboard.Dashboard.include({
         start: function(){
-            return this.load(['invitations'])
+            this.all_dashboards  = _.without(this.all_dashboards, 'planner', 'apps', 'share');
+            this.$('.o_web_settings_dashboard_apps').parent().remove();
+            this.$('.o_web_settings_dashboard_planner').parent().remove();
+            this.$('.o_web_settings_dashboard_share').parent().remove();
+            return this._super();
         },
     });
 });
