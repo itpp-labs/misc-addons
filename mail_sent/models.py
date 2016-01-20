0be0f0d6432947ade43f98335e3c35ed20f412e3
Ivan Yelizariev <yelizariev@it-projects.info>
2016-01-20 18:06:40 +0500

    fix pep errors

--- mail_sent/models.py
+++ mail_sent/models.py
@@ -1,4 +1,5 @@
-from openerp import api, models, fields, SUPERUSER_ID
+from openerp import api, models, fields
+
 
 class mail_message(models.Model):
     _inherit = 'mail.message'
@@ -6,9 +7,10 @@ class mail_message(models.Model):
     @api.one
     @api.depends('author_id', 'notified_partner_ids')
     def _get_sent(self):
-        self.sent = len(self.notified_partner_ids) > 1 or len(self.notified_partner_ids)==1 and self.author_id and self.notified_partner_ids[0].id != self.author_id.id
+        self.sent = len(self.notified_partner_ids) > 1 or len(self.notified_partner_ids) == 1 and self.author_id and self.notified_partner_ids[0].id != self.author_id.id
 
     sent = fields.Boolean('Sent', compute=_get_sent, help='Was message sent to someone', store=True)
+
 
 class mail_notification(models.Model):
     _inherit = 'mail.notification'
