823c2ee6915a37ac28bbf4fdf84d60be026a870b
Ivan Yelizariev <yelizariev@it-projects.info>
2016-01-20 17:46:31 +0500

    [FIX] error with empty author_id

--- mail_sent/models.py
+++ mail_sent/models.py
@@ -6,7 +6,7 @@ class mail_message(models.Model):
     @api.one
     @api.depends('author_id', 'notified_partner_ids')
     def _get_sent(self):
-        self.sent = len(self.notified_partner_ids) > 1 or len(self.notified_partner_ids)==1 and self.notified_partner_ids[0].id != self.author_id.id
+        self.sent = len(self.notified_partner_ids) > 1 or len(self.notified_partner_ids)==1 and self.author_id and self.notified_partner_ids[0].id != self.author_id.id
 
     sent = fields.Boolean('Sent', compute=_get_sent, help='Was message sent to someone', store=True)
 
