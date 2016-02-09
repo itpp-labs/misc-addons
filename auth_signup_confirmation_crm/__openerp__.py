f99429461deabe025745d3cb5788c2f9c4059f48
Ilyas <rakhimkulov@it-projects.info>
2016-02-09 17:18:00 +0500

    [ADD] new modules auth_signup_confirmation and auth_signup_confirmation_crm

---
+++ auth_signup_confirmation_crm/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Auth signup confirmation CRM',
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC',
+    'website': "https://it-projects.info",
+    'license': 'GPL-3',
+    'depends': [
+        'auth_signup_confirmation',
+        'crm',
+    ],
+    'installable': True,
+}
