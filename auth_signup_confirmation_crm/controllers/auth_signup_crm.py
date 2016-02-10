0f3592b51ff397909414d57cffb00a1b34d3fff8
Ilyas <rakhimkulov@it-projects.info>
2016-02-10 17:38:02 +0500

    - no duplicating leads

--- auth_signup_confirmation_crm/controllers/auth_signup_crm.py
+++ auth_signup_confirmation_crm/controllers/auth_signup_crm.py
@@ -7,9 +7,10 @@ import werkzeug
 class AuthLead(AuthConfirm):
     @http.route('/web/signup/confirm', type='http', auth='public')
     def singnup_using_generated_link(self, *args, **kw):
+        user_state_before = request.env['res.users'].sudo().search([('partner_id.signup_token', '=', kw['token'])]).active
         res = super(AuthLead, self).singnup_using_generated_link(*args, **kw)
         user = request.env['res.users'].sudo().search([('partner_id.signup_token', '=', kw['token'])])
-        if user:
+        if user.active and not user_state_before:
             new_lead = request.env['crm.lead'].sudo().create(
                  {
                     'name': user.partner_id.name,
