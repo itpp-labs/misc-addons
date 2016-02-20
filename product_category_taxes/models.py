0c55faae64fe4e122df1b7af92df820ae7c665c1
ufaks <excellm@yandex.ru>
2016-02-20 16:30:16 +0500

    fix

--- product_category_taxes/models.py
+++ product_category_taxes/models.py
@@ -4,21 +4,35 @@ from openerp import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    @api.model
+    def _get_new_taxes(self, categ_id):
+        print 'categ_id', categ_id
+        new_category = self.env['product.category'].browse(categ_id)
+        new_taxes = {
+            'taxes_id': [(6, 0, new_category.taxes_id.ids)],
+            'supplier_taxes_id': [(6, 0, new_category.supplier_taxes_id.ids)]
+        }
+        return new_taxes
+
     @api.onchange('categ_id')
     def _update_product_category_taxes(self):
         if self.categ_id:
-            new_category = self.env['product.category'].browse(self.categ_id.id)
-            self.taxes_id = [(6, 0, new_category.taxes_id.ids)]
-            self.supplier_taxes_id = [(6, 0, new_category.supplier_taxes_id.ids)]
+            new_taxes = self._get_new_taxes(self.categ_id.id)
+            self.taxes_id = new_taxes['taxes_id']
+            self.supplier_taxes_id = new_taxes['supplier_taxes_id']
+
+    @api.model
+    def create(self, vals):
+        if vals.get('categ_id'):
+            new_taxes = self._get_new_taxes(vals['categ_id'])
+            vals.update(new_taxes)
+        return super(ProductTemplate, self).create(vals)
 
     @api.multi
     def write(self, vals):
         if vals.get('categ_id'):
-            new_category = self.env['product.category'].browse(vals['categ_id'])
-            vals.update({
-                'taxes_id': [(6, 0, new_category.taxes_id.ids)],
-                'supplier_taxes_id': [(6, 0, new_category.supplier_taxes_id.ids)],
-            })
+            new_taxes = self._get_new_taxes(vals['categ_id'])
+            vals.update(new_taxes)
         return super(ProductTemplate, self).write(vals)
 
 
