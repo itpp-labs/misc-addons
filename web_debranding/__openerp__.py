6cc2562ebdd57ba3a8726bc607ff4420d3a924e6
Ivan Yelizariev <yelizariev@it-projects.info>
2016-02-27 15:57:44 +0500

    [DOC] web_debranding: bump 1.0.5 version

--- web_debranding/__openerp__.py
+++ web_debranding/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Backend debranding",
-    'version': '1.0.0',
+    'version': '1.0.5',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'GPL-3',
     'category': 'Debranding',
