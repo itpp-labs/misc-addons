792991957d6eb690d27aff99859d24268edd034b
ilmir <ilmir.karamov@gmail.com>
2016-02-29 13:36:06 +0500

    project_task_search_custom (edit)

--- project_task_search_custom/__openerp__.py
+++ project_task_search_custom/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 {
 'name': "Task Search Custom",
-'summary': """Searching for tasks includes "Assigned to" and "Description" fields""",
-'author': "Ilmir Karamov",
+'summary': """Searching for tasks includes "Messages" and "Description" fields""",
+'author': "IT-Projects LLC, Ilmir Karamov",
 'website': "http://it-projects.info",
 'version': '1.0.0',
 'category': 'Project',
