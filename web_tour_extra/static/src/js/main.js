8b2d9291c4ba86d7f7cda0d2069e30fc423fbaa6
Ilyas <rakhimkulov@it-projects.info>
2016-04-08 13:16:45 +0500

    [ADD] new module web_tour_extra

---
+++ web_tour_extra/static/src/js/main.js
@@ -0,0 +1,36 @@
+openerp.web_tour_extra = function(instance) {
+	var Tour = instance.web.Tour;
+
+	instance.web.Tour.getState = function () {
+		var state = JSON.parse(localStorage.getItem("tour") || 'false') || {};
+		if (state) {
+			this.time = state.time;
+		}
+		var tour_id, mode, step_id;
+		// tutorial_extra instead of tutorial
+		if (!state.id && window.location.href.indexOf("#tutorial_extra.") > -1) {
+			this.tutorial_extra = true;
+			state = {
+				"id": window.location.href.match(/#tutorial_extra\.(.*)=true/)[1],
+				"mode": "tutorial",
+				"step_id": 0
+			};
+			// Do not clean hash for reasons of saving starting tour path.
+			// window.location.hash = ""; 
+			Tour.log("Tour '" + state.id + "' Begin from url hash");
+			Tour.saveState(state.id, state.mode, state.step_id, 0);
+		}
+		if (!state.id) {
+			return;
+		}
+		state.tour = Tour.tours[state.id];
+		state.step = state.tour && state.tour.steps[state.step_id === -1 ? 0 : state.step_id];
+		return state;
+	};
+	$(document).ready(function () {
+    if (Tour.autoRunning) {
+        Tour.running();
+    };
+});
+
+};
