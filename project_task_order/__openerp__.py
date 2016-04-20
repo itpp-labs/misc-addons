49e9807b9844b22552c6fb2ab3ff9443ea5a372d
ilmir <ilmir.karamov@gmail.com>
2016-04-20 17:37:53 +0500

    Project task order module

--- project_task_order/__openerp__.py
+++ project_task_order/__openerp__.py
@@ -4,9 +4,13 @@
     'summary': """Order tasks by kanban state: 
 		  Normal, Done, Blocked""",
     'author': "IT-Projects LLC, Ilmir Karamov",
+    'license': 'GPL-3',
     'website': "http://it-projects.info",
     'category': 'Project',
     'version': '1.0.0',
+    'price': 15.00,
+    'currency': 'EUR',
+    'images': ['images/task_order.png'],
     'depends': ['project'],
     'installable': True
 }
