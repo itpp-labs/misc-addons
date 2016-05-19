68bc502ff561ee4ede0a3e56ecb9b6d5f043e495
Ivan Yelizariev <yelizariev@it-projects.info>
2016-05-19 20:46:27 +0500

    [DOC] base_session_store_psql

---
+++ base_session_store_psql/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Store sessions in postgresql""",
+    "summary": """Fixes "Session Expired" issue in destributed deployment""",
+    "category": "Extra Tools",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "website": "https://it-projects.info",
+    "license": "AGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ base_session_store_psql/doc/index.rst
@@ -0,0 +1,18 @@
+==============================
+ Store sessions in postgresql
+==============================
+
+Installation
+============
+
+Simply add this module to *addons path*. In current implementation, you even don't need to install it.
+
+Usage
+=====
+
+Module use the same db settings as odoo does.
+
+Uninstallation
+==============
+
+You have to remove this module from *addons path*, otherwise module still affect on odoo.
