9ea4b9680111e2472d6c7dc15a0792bbfc0a01bf
Ivan Yelizariev <yelizariev@it-projects.info>
2016-05-20 00:38:07 +0500

    [FIX] import issue

--- base_session_store_psql/__init__.py
+++ base_session_store_psql/__init__.py
@@ -1,7 +1,7 @@
 import openerp
 def post_load():
     if not openerp.tools.config.get('log_db'):
-        raise exceptions.UserError(
+        raise openerp.exceptions.UserError(
                 'You have to define a log_db value in the config to use the'
                 'Postgres Session Store.')
 
