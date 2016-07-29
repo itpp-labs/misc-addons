1209fd5dc5e15716291c99514c115ab9dbe61977
x620 <romanchenko@it-projects.info>
2016-07-29 13:51:11 +0500

    [ADD] Init commit for project_gantt8 and web_gantt8 modules: docs and manifest files

---
+++ project_gantt8/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Gantt view for Projects""",
+    "summary": """Restores feature from odoo 8.0""",
+    "category": "Project",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+        "project",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
