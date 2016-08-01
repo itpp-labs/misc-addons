8ee0121f43634ceabb63dd371de6380ccb77f94c
Noreddine Ben Jillali <nbj.microcom@gmail.com>
2016-08-01 15:29:00 -0400

    Migrate to 9

--- crm_expected_revenue/__openerp__.py
+++ crm_expected_revenue/__openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Multiply  expected revenue by probability",
-    'version': '1.0.0',
+    'version': '9.0.1.0.0',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Custom',
