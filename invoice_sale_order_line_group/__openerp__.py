7e5d66e00d6bc394ea5e2816c0ad7e83fcf8caf9
iledarn <nasyrov@it-projects.info>
2016-10-13 13:12:03 +0500

    [ADD] invoice_sale_order_line_group (#290)
    * [ADD] invoice_sale_order_line_group: new module
    
    * [DOC] invoice_sale_order_line_group
    
    * [DOC] invoce_sale_order_line_group
    
    * [FIX] invoice_sale_order_line_group: fix lint errors

---
+++ invoice_sale_order_line_group/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Invoice multiorder lines""",
+    "summary": """Choose different sale order lines from list and create one invoice per partner from them""",
+    "category": "Accounting",
+    "images": ['images/lines_to_invoice.png'],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ildar Nasyrov",
+    "website": "https://it-projects.info",
+    "license": "GPL-3",
+    "price": 50.00,
+    "currency": "EUR",
+
+    "depends": [
+        "sale",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/invoice_sale_order_line_group.xml",
+        "wizard/create_grouped_invoice.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
