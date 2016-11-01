f9ca6bd9711ead294bc954ce59d0369d9fab4550
Ivan Yelizariev <yelizariev@it-projects.info>
2016-11-01 13:17:46 +0500

    [ADD] copy-past original force_storage

--- ir_attachment_force_storage/models.py
+++ ir_attachment_force_storage/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from openerp import api
 from openerp import models
+from openerp.exceptions import AccessError
+from openerp.tools.translate import _
 
 
 class IrConfigParameter(models.Model):
@@ -23,3 +25,23 @@ class IrConfigParameter(models.Model):
         if self.key == 'ir_attachment.location':
             self._attachment_force_storage()
         return res
+
+
+class ir_attachment(models.Model):
+    _inherit = 'ir.attachment'
+
+    def force_storage(self, cr, uid, context=None):
+        """Force all attachments to be stored in the currently configured storage"""
+        if not self.pool['res.users'].has_group(cr, uid, 'base.group_erp_manager'):
+            raise AccessError(_('Only administrators can execute this action.'))
+
+        location = self._storage(cr, uid, context)
+        domain = {
+            'db': [('store_fname', '!=', False)],
+            'file': [('db_datas', '!=', False)],
+        }[location]
+
+        ids = self.search(cr, uid, domain, context=context)
+        for attach in self.browse(cr, uid, ids, context=context):
+            attach.write({'datas': attach.datas})
+        return True
