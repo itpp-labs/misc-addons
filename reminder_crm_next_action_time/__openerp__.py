597939c871d4802d33148a614c49516f26572cbd
ilmir <ilmir.karamov@gmail.com>
2016-11-04 20:02:54 +0500

    [ADD] copy-paste reminder_crm_next_action

---
+++ reminder_crm_next_action_time/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Reminders and Agenda for Opportunities",
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'license': 'GPL-3',
+    'category': 'Reminders and Agenda',
+    'website': 'https://twitter.com/yelizariev',
+    'price': 21.00,
+    'currency': 'EUR',
+    'depends': ['reminder_base', 'crm'],
+    'data': [
+        'views.xml',
+    ],
+    'installable': True,
+}
