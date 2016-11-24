1934987085a630e48c9f594729e9c0ceb03b79f0
ilmir <ilmir.karamov@gmail.com>
2016-11-24 18:18:07 +0500

    [DOC] reminder_crm_next_action_time

--- reminder_crm_next_action_time/__openerp__.py
+++ reminder_crm_next_action_time/__openerp__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Reminders and Agenda for Opportunities (with time)",
+    'summary': "Reminders for opportunities with the precise time feature",
     'version': '1.0.0',
+    'application': False,
     'author': 'IT-Projects LLC, Dinar Gabbasov',
     'license': 'GPL-3',
     'category': 'Reminders and Agenda',
@@ -12,5 +14,10 @@
     'data': [
         'views.xml',
     ],
+    'post_load': None,
+    'pre_init_hook': None,
+    'post_init_hook': None,
+
+    'auto_install': False,
     'installable': True,
 }
