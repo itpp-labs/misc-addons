4dd2fe4804300b19295804a610b440e2797e2dc7
Dinar <gabbasov@it-projects.info>
2016-11-24 13:42:05 +0500

    [ADD] new field datetime_action (#157)

--- reminder_crm_next_action_time/__openerp__.py
+++ reminder_crm_next_action_time/__openerp__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Reminders and Agenda for Opportunities",
+    'name': "Reminders and Agenda for Opportunities (with time)",
     'version': '1.0.0',
-    'author': 'IT-Projects LLC, Ivan Yelizariev',
+    'author': 'IT-Projects LLC, Dinar Gabbasov',
     'license': 'GPL-3',
     'category': 'Reminders and Agenda',
-    'website': 'https://twitter.com/yelizariev',
+    'website': 'https://twitter.com/gabbasov_dinar',
     'price': 21.00,
     'currency': 'EUR',
     'depends': ['reminder_base', 'crm'],
--- reminder_crm_next_action_time/models.py
+++ reminder_crm_next_action_time/models.py
@@ -6,6 +6,7 @@ from openerp import models
 class CrmLead(models.Model):
     _name = 'crm.lead'
     _inherit = ['crm.lead', 'reminder']
-    _reminder_date_field = 'date_action'
+    datetime_action = fields.Datetime(string='Next Action Datetime')
+    _reminder_date_field = 'datetime_action'
     _reminder_description_field = 'title_action'
     reminder_alarm_ids = fields.Many2many(string='Next Action Reminders')
