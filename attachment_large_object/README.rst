66f06984f28516cb253e02d6dee3809807819a4a
Ivan Yelizariev <yelizariev@it-projects.info>
2016-11-29 13:11:34 +0500

    [ADD] apply updates from 8.0 branch of https://bitbucket.org/anybox/advanced_attachment/overview

--- attachment_large_object/README.rst
+++ attachment_large_object/README.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-advanced_attachment addons for OpenERP 7.0
+advanced_attachment addons for OpenERP 8.0
 ==========================================
 
 These addons enhance OpenERP's native attachment system with several
@@ -45,7 +45,7 @@ performance. That being said, large objects
 
 How to use
 ----------
-Once the module is installed,
+**Once the module is installed**,
 to have ``ir_attachment`` records been stored as large objects by
 default, set the following record in ``ir.config_parameter``:
 
@@ -53,6 +53,10 @@ default, set the following record in ``ir.config_parameter``:
 :value: ``postgresql:lobject``
 
 Preexisting attachments are unaffected.
+
+.. warning:: If you do that setting before installing the module
+             you will get inconsistent data in ``ir.attachment``
+             field ``is_lobject`` will be set to ``True``.
 
 .. note :: for various compatibility reasons, this implementation still does
            the same on-the-fly base64 encoding and decoding as the
@@ -63,6 +67,76 @@ Preexisting attachments are unaffected.
            application part, not on the database system.
            A later version might bring more options.
 
+Data recovery procedure
+-----------------------
+
+To transfer your data from *file system* or from *base64 encoded text column*
+to *large object* you can use the ``convert_attachment`` script.
+
+This will basicly read the attachment and save it as ``lobject``.
+
+Install from buildout
+~~~~~~~~~~~~~~~~~~~~~
+
+Here a basic config to install script in your environment using buildout,
+``buildout.cfg`` file looks like::
+
+    [buildout]
+    parts = odoo
+    versions = versions
+    find-links = http://download.gna.org/pychart/
+    extensions = gp.vcsdevelop
+    vcs-extend-develop = hg+https://bitbucket.org/anybox/advanced_attachment@8.0#egg=advanced.attachment.scripts
+
+    [odoo]
+    recipe = anybox.recipe.odoo:server
+    version = git https://github.com/OCA/OCB.git ocb 8.0
+    addons = hg https://bitbucket.org/anybox/advanced_attachment community-addons/advanced_attachment 8.0
+
+    openerp_scripts = convert_attachment=convert_attachment arguments=session
+
+    eggs =
+        PyPDF
+        advanced.attachment.scripts
+
+    [versions]
+    ...
+
+
+Usage
+~~~~~
+Assuming you install this script with the upper buildout config::
+
+    bin/convert_attachment --help
+    usage: convert_attachment [-h] [-d DATABASE] [-N TRANSACTION_NUMBER]
+                              [-F MAX_FILE_SIZE] [-p ODOO_DATA_DIR] [-s] [-f]
+
+    Script to upgrade odoo attachment to large object attachment.
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -d DATABASE, --database DATABASE
+                            Database to work on
+      -N TRANSACTION_NUMBER, --transaction-number TRANSACTION_NUMBER
+                            Tell how often commit. Default, commit every 50
+                            attachments
+      -F MAX_FILE_SIZE, --max-file-size MAX_FILE_SIZE
+                            Max file size to upgrade (in octets)
+      -p ODOO_DATA_DIR, --odoo-data-dir ODOO_DATA_DIR
+                            Odoo filestore directory, default
+                            /home/you/.local/share/Odoo.
+      -s, --simulate        Simulation mode: just logs actions but does not
+                            perform.
+      -f, --force           Do not raise if attachment data is missing,carry on
+                            silently
+
+``-F`` and ``-N`` options are there to let you manage memory usage when
+transfer data to avoid memory error.
+
+.. note:: the simulate mode does not perform any database write (it's
+          not only doing rollbacks), because that would actually remove the
+          file from disk. You can't have a reliable estimation of
+          execution time through it.
 
 Indices and tables
 ==================
--- attachment_large_object/ir_attachment.py
+++ attachment_large_object/ir_attachment.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import logging
-from openerp.osv import osv
+from openerp.osv import osv, fields
+from openerp import SUPERUSER_ID
+import psycopg2
 
 logger = logging.getLogger(__name__)
 
@@ -22,43 +23,105 @@ class IrAttachment(osv.Model):
     def lobject(self, cr, *args):
         return cr._cnx.lobject(*args)
 
-    def _file_write(self, cr, uid, location, value):
+    def _file_write(self, cr, uid, value):
         """Write the content in a newly created large object.
 
         :param value: base64 encoded payload
         :returns str: object id (will be considered the file storage name)
         """
-
+        location = self._storage(cr, uid)
         if location != LARGE_OBJECT_LOCATION:
-            return super(IrAttachment, self)._file_write(cr, uid, location, value)
+            return super(IrAttachment, self)._file_write(cr, uid, value)
 
         lobj = self.lobject(cr, 0, 'wb')  # oid=0 means creation
         lobj.write(value.decode('base64'))
         oid = lobj.oid
         return str(oid)
 
-    def _file_delete(self, cr, uid, location, fname):
+    def _lobject_delete(self, cr, uid, fname):
         """Delete the large object.
 
         :param fname: file storage name, must be the oid as a string.
         """
-        if location != LARGE_OBJECT_LOCATION:
-            return super(IrAttachment, self)._file_delete(cr, uid, location, fname)
+        try:
+            oid = long(fname)
+        except:
+            # it means there is a legacy attachment
+            self._file_delete(cr, uid, fname)
+        else:
+            self.lobject(cr, oid, 'rb').unlink()
 
-        self.lobject(cr, long(fname), 'rb').unlink()
-
-    def _file_read(self, cr, uid, location, fname, bin_size=False):
+    def _lobject_read(self, cr, uid, fname, bin_size):
         """Read the large object, base64 encoded.
 
         :param fname: file storage name, must be the oid as a string.
         """
-        if location != LARGE_OBJECT_LOCATION:
-            return super(IrAttachment, self)._file_read(cr, uid, location, fname, bin_size=bin_size)
-
         lobj = self.lobject(cr, long(fname), 'rb')
         if bin_size:
             return lobj.seek(0, 2)
         return lobj.read().encode('base64')  # GR TODO it must be possible to read-encode in chunks
 
+    def _data_get(self, cr, uid, ids, name, arg, context=None):
+        if context is None:
+            context = {}
+        result = {}
+        bin_size = context.get('bin_size')
+        for attach in self.browse(cr, uid, ids, context=context):
+            try:
+                result[attach.id] = self._lobject_read(cr, uid, attach.store_fname, bin_size)
+            except (psycopg2.OperationalError, ValueError):
+                if attach.store_fname:
+                    result[attach.id] = self._file_read(cr, uid, attach.store_fname, bin_size)
+                else:
+                    result[attach.id] = attach.db_datas
+        return result
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    def _data_set(self, cr, uid, id, name, value, arg, context=None):
+        # We dont handle setting data to null
+        if not value:
+            return True
+        if context is None:
+            context = {}
+        location = self._storage(cr, uid, context)
+        file_size = len(value.decode('base64'))
+        attach = self.browse(cr, uid, id, context=context)
+        fname_to_delete = attach.store_fname
+        if location != 'db':
+            fname = self._file_write(cr, uid, value)
+            # SUPERUSER_ID as probably don't have write access, trigger during create
+            vals = {
+                'store_fname': fname,
+                'file_size': file_size,
+                'db_datas': False,
+            }
+            super(IrAttachment, self).write(cr, SUPERUSER_ID, [id], vals, context=context)
+        else:
+            super(IrAttachment, self).write(
+                cr, SUPERUSER_ID, [id],
+                {'db_datas': value, 'file_size': file_size, 'store_fname': False},
+                context=context)
+
+        # ugly but the write delete the file_size entry
+        query = 'update %s set file_size=%%s where id = %%s' % self._table
+        cr.execute(query, (file_size, id))
+
+        # After de-referencing the file in the database, check whether we need
+        # to garbage-collect it on the filesystem
+        if fname_to_delete:
+            try:
+                self._lobject_delete(cr, uid, fname_to_delete)
+            except (psycopg2.OperationalError, ValueError):
+                self._file_delete(cr, uid, fname_to_delete)
+        return True
+
+    _columns = {
+        'datas': fields.function(
+            _data_get, fnct_inv=_data_set, string='File Content', type="binary", nodrop=True),
+    }
+
+    def create(self, cr, uid, values, context=None):
+        # ugly but correct a base module
+        if 'file_size' in values:
+            del values['file_size']
+
+        return super(IrAttachment, self).create(cr, uid, values, context=context)
