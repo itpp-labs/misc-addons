421afbf9a14b5a98cfcf4d3e5c14f30c6e0d0645
Dinar <gabbasov@it-projects.info>
2016-12-06 12:17:18 +0500

    [ADD] base_import_map (#160)

---
+++ base_import_map/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Base Import Map",
+    "version": "1.0.0",
+    "summary": "Base Import Map",
+    "category": "Extra Tools",
+
+    "author": "IT-Projects LLC, Dinar Gabbasov",
+    "website": "https://www.twitter.com/gabbasov_dinar",
+
+    "depends": ["base_import"],
+    "data": [
+        "views/base_import_map_templates.xml",
+        "views/base_import_map_view.xml",
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ base_import_map/static/src/js/base_import_map.js
@@ -0,0 +1,57 @@
+odoo.define('base_import_map.map', function (require) {
+    "use strict";
+
+    var ControlPanelMixin = require('web.ControlPanelMixin');
+    var BaseImport = require('base_import.import');
+    var core = require('web.core');
+    var Model = require('web.Model');
+
+    var QWeb = core.qweb;
+    var _lt = core._lt;
+    var _t = core._t;
+
+    BaseImport.DataImport.include({
+        opts: [
+            {name: 'encoding', label: _lt("Encoding:"), value: 'utf-8'},
+            {name: 'separator', label: _lt("Separator:"), value: ','},
+            {name: 'quoting', label: _lt("Text Delimiter:"), value: '"'},
+            {name: 'settings', label: _lt("Settings:"), value: ''},
+            {name: 'save_settings', label: _lt("Save settings:"), value: ''},
+            {name: 'file_read_hook', label: _lt("File read hook:"), value: ''}
+        ],
+        start: function() {
+            var self = this;
+            this.setup_settings_picker();
+            this._super();
+        },
+        setup_settings_picker: function(){
+            var self = this;
+            var domain = [['model', '=', this.res_model]];
+            var model = new Model("base_import_map.map");
+            model.call('name_search', {
+                args: domain || false,
+            }).then(function(res){
+                var suggestions = [];
+                if (res) {
+                    res.forEach(function (item) {
+                        suggestions.push({id: item[0], text: _t(item[1])});
+                    });
+                } else {
+                    suggestions.push({id: "None", text: _t("None")});
+                }
+                self.$('input.oe_import_settings').select2({
+                    width: '160px',
+                    query: function (q) {
+                        if (q.term) {
+                            suggestions.unshift({id: q.term, text: q.term});
+                        }
+                        q.callback({results: suggestions});
+                    },
+                    initSelection: function (e, c) {
+                        return c({id: "None", text: _t("None")});
+                    },
+                });
+            });
+        },
+    });
+});
