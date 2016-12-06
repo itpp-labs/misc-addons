d3e0d89ffb8eeb4e1dcf0df406dd2ddf81694c34
Ivan Yelizariev <yelizariev@it-projects.info>
2016-12-06 14:43:29 +0500

    [DOC][FIX] add doc, different fixed and improvements

--- base_import_map/__manifest__.py
+++ base_import_map/__manifest__.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Base Import Map",
+    "name": "Import Settings",
     "version": "1.0.0",
-    "summary": "Base Import Map",
+    "summary": "Allows to save import settings to don't specify columns to fields mapping each time.",
     "category": "Extra Tools",
+    "images": ["images/icon.png"],
 
     "author": "IT-Projects LLC, Dinar Gabbasov",
     "website": "https://www.twitter.com/gabbasov_dinar",
+    "license": "LGPL-3",
+    "price": 90.00,
+    "currency": "EUR",
 
     "depends": ["base_import"],
     "data": [
+        "security/ir.model.access.csv",
         "views/base_import_map_templates.xml",
         "views/base_import_map_view.xml",
     ],
