a1680432755d96a1f6e5f9fc9b32ca6632d359f7
GabbasovDinar <gabbasov@it-projects.info>
2017-01-10 15:45:36 +0500

    [FIX] Error FIX

--- web_debranding/static/src/js/dialog.js
+++ web_debranding/static/src/js/dialog.js
@@ -13,7 +13,7 @@ odoo.define('web_debranding.dialog', function(require) {
             var debranding_new_name = odoo.debranding_new_name;
             var debranding_new_website = odoo.debranding_new_website;
             options = options || {};
-            if (options.title){
+            if (options.title && options.title.replace){
                 var title = options.title.replace(/Odoo/ig, debranding_new_name);
                 options.title = title;
             } else {
