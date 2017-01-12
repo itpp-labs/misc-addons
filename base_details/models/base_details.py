97eea52e21c2902a38e72eff09e85deb3e809a4c
ufaks <excellm@yandex.ru>
2017-01-12 17:07:56 +0500

    [ADD] new module base_details, change the product_details module

---
+++ base_details/models/base_details.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class BaseDetails(models.AbstractModel):
+    _name = 'base_details'
+
+    def _model_selection(self):
+        return []
+
+    @property
+    def get_details(self):
+        if self.details_model and self.details_model in self.env and self.details_res_id:
+            details_record = self.env[self.details_model].browse(self.details_res_id)
+        else:
+            details_record = None
+        return details_record
+
+    details_model = fields.Selection('_model_selection', string='Detail Model')
+    details_res_id = fields.Integer(string='Details')
--- product_details/models/product_detail.py
+++ product_details/models/product_detail.py
@@ -1,26 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    @api.model
-    def _selection_details(self):
-        return []
-
-    details = fields.Reference(
-        selection='_selection_details', string='Details',
-        help="You can choose a source where to store product details")
-
-
-class ProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
-
-    @api.model
-    def _selection_details(self):
-        return []
-
-    details = fields.Reference(
-        selection='_selection_details', string='Details',
-        help="You can choose a source where to store lot/serial number details")
+    _name = 'product.product'
+    _inherit = ['product.product', 'base_details']
