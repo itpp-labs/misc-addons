cef7da024963741c8d50274c6553c6312f9d654d
GabbasovDinar <gabbasov@it-projects.info>
2017-01-20 16:49:33 +0500

    [PORT] fixes porting errors from 8.0 to 9.0

--- reminder_crm_next_action_time/__openerp__.py
+++ reminder_crm_next_action_time/__openerp__.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Reminders and Agenda for Opportunities (with time)",
-    'summary': "Reminders for opportunities with the precise time feature",
-    'version': '1.0.0',
-    'application': False,
-    'author': 'IT-Projects LLC, Dinar Gabbasov',
-    'license': 'GPL-3',
-    'category': 'Reminders and Agenda',
-    'website': 'https://twitter.com/gabbasov_dinar',
-    'price': 21.00,
-    'currency': 'EUR',
-    'depends': ['reminder_base', 'crm'],
-    'data': [
-        'views.xml',
-    ],
-    'post_load': None,
-    'pre_init_hook': None,
-    'post_init_hook': None,
+    "name": """Reminders and Agenda for Opportunities (with time)""",
+    "summary": """Reminders for opportunities with the precise time feature""",
+    "category": "Debranding",
+    "images": [],
+    "version": "1.1.0",
+    "application": False,
 
-    'auto_install': False,
-    'installable': False,
+    "author": "IT-Projects LLC, Dinar Gabbasov",
+    "website": "https://twitter.com/gabbasov_dinar",
+    "license": "GPL-3",
+    "price": 21.00,
+    "currency": "EUR",
+
+    "depends": [
+        "reminder_base",
+        "crm",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views.xml",
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
 }
