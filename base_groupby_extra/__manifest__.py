d6370e4049978f3972629ba1289b666b307e6dcf
Ivan Yelizariev <yelizariev@it-projects.info>
2017-02-02 13:38:08 +0500

    [ADD] 10.0 version of base_groupby_extra

---
+++ base_groupby_extra/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Extra grouping options""",
+    "summary": """Adds grouping by hours""",
+    "category": "Extra Tools",
+    "images": [],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
