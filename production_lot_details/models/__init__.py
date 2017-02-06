aef7a7a4c83f5fec0037384e025ee6f8e00bebdb
ufaks <excellm@yandex.ru>
2017-02-06 18:51:19 +0500

    [ADD] new module production_lot_details (#366)

---
+++ production_lot_details/models/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import production_lot_detail
---
+++ production_lot_details/models/production_lot_detail.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+
+class ProductionLot(models.Model):
+    _name = 'stock.production.lot'
+    _inherit = ['stock.production.lot', 'base_details']
