9c08282d7d158ad1340fc471f463909f6d3093cc
Ivan Yelizariev <yelizariev@it-projects.info>
2017-02-21 17:39:56 +0500

    [PORT] fix barcode_widget
    [IMP] show barcode value in readonly mode

--- barcode_widget/static/src/js/barcode_widget.js
+++ barcode_widget/static/src/js/barcode_widget.js
@@ -9,11 +9,16 @@ odoo.define('barcode_widget.form_widgets', function (require) {
         template: 'Barcode128Widget',
         render_value: function () {
             var show_value = this.format_value(this.get('value'), '');
-            var barcode_path = '/report/barcode/?type=Code128&value=' + show_value +'&width=250&height=50';
-            var $barcodelink = this.$el.find('a');
-            $barcodelink.attr("href", barcode_path);
-            var $barcode = this.$el.find('img');
-            $barcode.attr("src", barcode_path);
+            if (this.$input) {
+                this.$input.val(show_value);
+            }else{
+                this.$el.find('span').text(show_value);
+                var barcode_path = '/report/barcode/Code128/' + show_value +'?width=250&height=50';
+                var $barcodelink = this.$el.find('a');
+                $barcodelink.attr("href", barcode_path);
+                var $barcode = this.$el.find('img');
+                $barcode.attr("src", barcode_path);
+            }
         }
     });
     core.form_widget_registry.add('BarCode128', Barcode128Widget);
