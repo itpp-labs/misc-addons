397a140127d70a95032fc7bcef5fe5c2b766b114
ilmir <ilmir.karamov@gmail.com>
2017-03-16 14:51:20 +0500

    [PORT] Migrated res_partner_skype module to v10

--- res_partner_skype/static/src/js/skype.js
+++ res_partner_skype/static/src/js/skype.js
@@ -7,30 +7,15 @@ var QWeb = instance.web.qweb;
 
 instance.web.form.FieldSkype = instance.web.form.FieldChar.extend({
     template: 'FieldSkype',
-    initialize_content: function() {
-        this._super();
-        var $button = this.$el.find('button');
-        $button.click(this.on_button_clicked);
-        this.setupFocus($button);
+    prefix: 'skype',
+    init: function() {
+        this._super.apply(this, arguments);
+        this.clickable = true;
     },
     render_value: function() {
-        if (!this.get("effective_readonly")) {
-            this._super();
-        } else {
-            this.$el.find('a')
-                    .attr('href', this.skype_uri())
-                    .text(this.get('value') || '');
-        }
-    },
-    skype_uri:function(){
-        //http://developer.skype.com/skype-uris/reference
-        return 'skype:' + this.get('value') + '?'+(this.options.type || 'chat') + (this.options.video?'&video=true':'') + (this.options.topic?'&topic='+encodeURIComponent(this.options.topic):'');
-    },
-    on_button_clicked: function() {
-        if (!this.get('value') || !this.is_syntax_valid()) {
-            this.do_warn(_t("Skype Error"), _t("Can't skype to invalid skype address"));
-        } else {
-            location.href = this.skype_uri();
+        this._super();
+        if (this.get("effective_readonly") && this.clickable) {
+            this.$el.attr('href', this.prefix + ':' + this.get('value') + '?'+(this.options.type || 'call'));
         }
     }
 });
