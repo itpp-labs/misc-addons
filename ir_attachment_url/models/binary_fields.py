1e73bbbb49de39b2d2a17736f7aa605262d64910
GabbasovDinar <gabbasov@it-projects.info>
2017-04-11 15:51:32 +0500

    [FIX] Fixes lint error

--- ir_attachment_url/models/binary_fields.py
+++ ir_attachment_url/models/binary_fields.py
@@ -44,7 +44,7 @@ class Binary(fields.Binary):
             super(Binary, self).write(records, value)
 
     def is_url(self, value):
-        return re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', value)
+        return re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', value)
 
 
 fields.Binary = Binary
