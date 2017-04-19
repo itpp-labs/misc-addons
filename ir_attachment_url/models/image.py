8fbd9db4ce9f492e7c558f1edd11a776b258421c
GabbasovDinar <gabbasov@it-projects.info>
2017-04-19 19:10:25 +0500

    [REF] Refactoring code

--- ir_attachment_url/models/image.py
+++ ir_attachment_url/models/image.py
@@ -3,42 +3,25 @@ from odoo import tools
 import re
 
 
+super_image_resize_images = tools.image_resize_images
+
+
 def image_resize_images(vals, big_name='image', medium_name='image_medium', small_name='image_small'):
     """ Update ``vals`` with image fields resized as expected. """
     url = None
-    if big_name in vals:
-        if not is_url(vals[big_name]):
-            vals.update(tools.image_get_resized_images(vals[big_name],
-                                                       return_big=True, return_medium=True, return_small=True,
-                                                       big_name=big_name, medium_name=medium_name,
-                                                       small_name=small_name,
-                                                       avoid_resize_big=True, avoid_resize_medium=False,
-                                                       avoid_resize_small=False))
-        else:
-            url = vals[big_name]
-    elif medium_name in vals:
-        if not is_url(vals[medium_name]):
-            vals.update(tools.image_get_resized_images(vals[medium_name],
-                                                       return_big=True, return_medium=True, return_small=True,
-                                                       big_name=big_name, medium_name=medium_name, small_name=small_name,
-                                                       avoid_resize_big=True, avoid_resize_medium=True,
-                                                       avoid_resize_small=False))
-        else:
-            url = vals[medium_name]
+    if big_name in vals and is_url(vals[big_name]):
+        url = vals[big_name]
+    elif medium_name in vals and is_url(vals[medium_name]):
+        url = vals[medium_name]
+    elif small_name in vals and is_url(vals[small_name]):
+        url = vals[small_name]
 
-    elif small_name in vals:
-        if not is_url(vals[small_name]):
-            vals.update(tools.image_get_resized_images(vals[small_name],
-                                                       return_big=True, return_medium=True, return_small=True,
-                                                       big_name=big_name, medium_name=medium_name, small_name=small_name,
-                                                       avoid_resize_big=True, avoid_resize_medium=True,
-                                                       avoid_resize_small=True))
-        else:
-            url = vals[small_name]
     if url:
         vals.update({big_name: url})
         vals.update({medium_name: url})
         vals.update({small_name: url})
+    else:
+        super_image_resize_images(vals, big_name, medium_name, small_name)
 
 
 def is_url(value):
