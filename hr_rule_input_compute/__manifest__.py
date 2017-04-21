94c230897dbad66ba889b15b2d3383237d5cd73f
ufaks <krotov@it-projects.info>
2017-04-21 13:05:50 +0500

    [ADD][10.0] new module "hr_rule_input_compute" (#412)
    * [ADD] new "hr_rule_input_compute" module
    
    * [IMP] made a "input_python_compute" field is editable for users with "group_hr_rule_input_compute" group only
    
    * [FIX] used position="attributes" instead replace field

---
+++ hr_rule_input_compute/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Compute Salary Inputs""",
+    "summary": """Allows to compute amounts of inputs in salary rules""",
+    "category": "Human Resources",
+    # "live_test_URL": "",
+    "images": ["static/description/icon.png"],
+    "version": "1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+        'hr_payroll',
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        'security/hr_rule_input_compute_security.xml',
+        'views/hr_payroll_views.xml',
+    ],
+    "qweb": [
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
