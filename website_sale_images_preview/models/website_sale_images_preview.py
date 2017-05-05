cb08daefc0269a8ff2e784304c5964a36c932953
ilmir <ilmir.karamov@gmail.com>
2017-05-05 15:16:47 +0500

    [REM] removed module website_sale_images_preview

--- website_sale_images_preview/models/website_sale_images_preview.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models
-
-
-class ProductImage(models.Model):
-    _name = 'product.image'
-    _inherit = ['product.image', 'web.preview']
-
-    _preview_media_file = 'image'
