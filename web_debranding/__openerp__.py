0744f6bdfc208e1e7db80cec28111761a1ac5be0
root <liliya@it-projects.info>
2017-05-23 09:19:40 +0000

    [DOC] web_debranding new logo 9.0

--- web_debranding/__openerp__.py
+++ web_debranding/__openerp__.py
@@ -5,6 +5,7 @@
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Debranding',
+    'images':['images/web_debranding.png'],
     'website': 'https://twitter.com/yelizariev',
     'price': 150.00,
     'currency': 'EUR',
