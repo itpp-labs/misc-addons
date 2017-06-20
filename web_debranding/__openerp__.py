8652f2d494f42bae24cc6b71c156d2d59e05ef29
Lily <liliya@it-projects.info>
2017-06-20 20:48:49 +0500

    [DOC] dump new version related to fixing title (#439)

--- web_debranding/__openerp__.py
+++ web_debranding/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Backend debranding",
-    'version': '1.0.18',
+    'version': '1.0.19',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Debranding',
