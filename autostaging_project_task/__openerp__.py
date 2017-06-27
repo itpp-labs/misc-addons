1ac8da2ef6c4791a65f589cf56d397bfeec15b22
ILMIR <karamov@it-projects.info>
2017-06-27 19:39:30 +0500

    [DOC] Update __openerp__.py

--- autostaging_project_task/__openerp__.py
+++ autostaging_project_task/__openerp__.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Autostaging project task",
-    'summary': "Change stages of tasks automatically after a specified time"
+    'summary': "Change stages of tasks automatically after a specified time",
     'author': "IT-Projects LLC, Ildar Nasyrov",
+    'support': "apps@it-projects.info",
     'license': 'LGPL-3',
-    'website': "https://twitter.com/nasyrov_ildar",
+    'website': "https://it-projects.info",
     'images': ['images/a.png'],
     'category': 'Project',
     'version': '1.0.1',
+    'application': False,
     'price': 39.00,
     'currency': 'EUR',
     'depends': ['project', 'autostaging_base'],
+    'external_dependencies': {"python": [], "bin": []},
     'data': [
         'views.xml',
     ],
-    "installable": True
+    
+    'post_load': None,
+    'pre_init_hook': None,
+    'post_init_hook': None,
+
+    'auto_install': False,
+    'installable': True,
 }
