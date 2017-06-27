c5e5bb35ce5c89059a76ed59571b6419e7e599ab
root <liliya@it-projects.info>
2017-06-27 12:24:40 +0000

    [DOC]new  logo and price

--- autostaging_project_task/__openerp__.py
+++ autostaging_project_task/__openerp__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Autostaging project task",
+    'summary': "Change stages of tasks automatically after a specified time"
     'author': "IT-Projects LLC, Ildar Nasyrov",
     'license': 'LGPL-3',
     'website': "https://twitter.com/nasyrov_ildar",
+    'images': ['images/a.png'],
     'category': 'Project',
     'version': '1.0.1',
+    'price': 39.00,
+    'currency': 'EUR',
     'depends': ['project', 'autostaging_base'],
     'data': [
         'views.xml',
