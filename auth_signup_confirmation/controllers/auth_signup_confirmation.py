119d9d876c45080047110173c866eeb36b284244
Ivan Yelizariev <yelizariev@it-projects.info>
2017-08-28 05:55:22 +0000

    [FIX] for unknown reason kw may not have 'redirect' key

--- auth_signup_confirmation/controllers/auth_signup_confirmation.py
+++ auth_signup_confirmation/controllers/auth_signup_confirmation.py
@@ -81,7 +81,7 @@ class AuthConfirm(AuthSignupHome):
             )._signup_create_user(values)
             new_user.active = False
             new_partner = new_user.partner_id
-        redirect_url = werkzeug.url_encode({'redirect': kw['redirect']})
+        redirect_url = werkzeug.url_encode({'redirect': kw.get('redirect') or '/web'})
         new_partner.signup_prepare()
         signup_url = new_partner.with_context(signup_force_type_in_url='signup/confirm',
                                               signup_valid=True).signup_url
