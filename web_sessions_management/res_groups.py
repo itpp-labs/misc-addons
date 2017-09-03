9ee2ddba329a527c1437e0c3229c044115b5759f
Nicolas JEUDY <nicolas@sudokeys.com>
2017-09-03 17:03:50 +0200

    [FIX] typo in help declaration

--- web_sessions_management/res_groups.py
+++ web_sessions_management/res_groups.py
@@ -33,11 +33,10 @@ class ResGroups(models.Model):
     login_calendar_id = fields.Many2one('resource.calendar'
                                              'Allow Login Calendar', company_dependent=True,
                                              help='The user will be only allowed to login in the calendar defined here.'),
-    no_multiple_sessions = fields.Boolean('No Multiple Sessions', company_dependent=True
+    no_multiple_sessions = fields.Boolean('No Multiple Sessions', company_dependent=True,
                                                help='Select this to prevent user to start a session more than once'),
     interval_number = fields.Integer('Session Timeout', company_dependent=True, help='Timeout since last activity for auto logout')
     interval_type = fields.Selection([('minutes', 'Minutes')
                                            ('hours', 'Hours'), ('work_days', 'Work Days'),
                                            ('days', 'Days'), ('weeks', 'Weeks'), ('months', 'Months')],
                                           'Interval Unit', company_dependent=True),
-
