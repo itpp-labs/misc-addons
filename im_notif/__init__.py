36562c3be21941b3673e45ac11006049d90e675f
Ildar Nasyrov <nasyrov@it-projects.info>
2017-09-25 12:57:46 +0000

    [REM] im_notif is relocated to mail-addons

--- im_notif/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import im_notif_models
-
-
-def pre_uninstall(cr, registry):
-    query = ("UPDATE res_partner "
-             "SET notify_email = 'always' "
-             "WHERE notify_email LIKE 'im%';")
-    cr.execute(query)
