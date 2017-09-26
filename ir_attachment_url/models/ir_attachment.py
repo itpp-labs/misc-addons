2910d8ece3220334fb7eaba6cc79adcb05720975
Ildar Nasyrov <nasyrov@it-projects.info>
2017-09-26 18:41:49 +0000

    [BACKPORT] ir_attachment_url: there was _data_get instead of _compute_datas in 9.0

--- ir_attachment_url/models/ir_attachment.py
+++ ir_attachment_url/models/ir_attachment.py
@@ -2,23 +2,41 @@
 import requests
 import base64
 from openerp import api, models
+from openerp.osv import fields, osv
+
+
+class ir_attachment(osv.osv):
+    _name = 'ir.attachment'
+    _inherit = ['ir.attachment']
+
+    def _data_set(self, cr, uid, id, name, value, arg, context=None):
+        return super(ir_attachment, self)._data_set(cr, uid, ids, name, arg, context=context)
+
+    def _data_get(self, cr, uid, ids, name, arg, context=None):
+        if context is None:
+            context = {}
+        result = {}
+        bin_size = context.get('bin_size')
+        ordinary_ids = []
+        for attach in self.browse(cr, uid, ids, context=context):
+            if attach.type == 'url' and attach.url:
+                if not bin_size:
+                    response = requests.get(attach.url)
+                    result[attach.id] = base64.b64encode(response.content)
+                else:
+                    result[attach.id] = "1.00 Kb"
+            else:
+                ordinary_ids.append(attach.id)
+        result.update(super(ir_attachment, self)._data_get(cr, uid, ordinary_ids, name, arg, context=context))
+        return result
+
+    _columns = {
+        'datas': fields.function(_data_get, fnct_inv=_data_set, string='File Content', type="binary", nodrop=True),
+    }
 
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
-
-    @api.depends('store_fname', 'db_datas')
-    def _compute_datas(self):
-        bin_size = self._context.get('bin_size')
-        url_records = self.filtered(lambda r: r.type == 'url' and r.url)
-        for attach in url_records:
-            if not bin_size:
-                r = requests.get(attach.url)
-                attach.datas = base64.b64encode(r.content)
-            else:
-                attach.datas = "1.00 Kb"
-
-        super(IrAttachment, self - url_records)._compute_datas()
 
     @api.multi
     def _filter_protected_attachments(self):
