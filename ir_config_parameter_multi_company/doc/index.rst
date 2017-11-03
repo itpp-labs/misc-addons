76d8b7653b4fe5db283ef30a0eeac85ad475d3d2
Ivan Yelizariev <yelizariev@it-projects.info>
2017-11-03 16:17:56 +0500

    [NEW] ir_config_parameter_multi_company (#489)

---
+++ ir_config_parameter_multi_company/doc/index.rst
@@ -0,0 +1,103 @@
+===============================================
+ Company-dependent values in System Parameters
+===============================================
+
+Installation
+============
+
+* `Install <https://odoo-development.readthedocs.io/en/latest/odoo/usage/install-module.html>`__ this module in a usual way
+* Make database backup (or at least ``ir.config_parameter`` table)
+
+Configuration
+=============
+
+* Open menu ``[[ Settings ]] >> General Settings``
+* Activate **[x] Multi Company - Manage multiple companies**
+* Click ``[Apply]``
+* Open menu ``[[ Settings ]] >> Users >> Users``
+* Select your user
+* Add some companies to **Allowed Companies** field
+
+Usage
+=====
+
+* `Activate Developer Mode <https://odoo-development.readthedocs.io/en/latest/odoo/usage/debug-mode.html>`__
+* Open menu ``[[ Settings ]] >> Technical >> Parameters >> System Parameters``
+* Choose some record or create new one
+* Set some value
+* Switch your user to another company
+
+  * Click top right-hand button with your user name
+  * Click ``Preferences``
+  * Set new value for **Company** field
+  * Click ``[Save]``
+
+* Set new value for the System Parameter record
+* Switch back to previous company
+* RESULT: Value of the System Parameter depends on current company 
+
+Default values
+--------------
+
+All system parameters craeted before module installation (as well as just created parameters) become default value for corresponding parameters. Example:
+
+* Before installation:
+
+  * **param1** = *value1*
+  * **param2** = *value2*
+
+* After installation:
+
+  * **param1** = *value1* -- default value for param1
+  * **param2** = *value2* -- default value for param2
+
+* Now if we switch to company1 and make following updates:
+
+  * **param1** = *value11*
+
+* And if we switch to companyB and make following updates:
+
+  * **param2** = *value22*
+  * **param3** = *value3*
+
+* Then for companyA we have
+
+  * **param1** = *value11* (value for companyA)
+  * **param2** = *value2* (default value)
+  * **param3** = *value3* (default value)
+
+* Then for companyA we have
+
+  * **param1** = *value1* (via default value)
+  * **param2** = *value22* (value for companyB)
+  * **param3** = *value3* (via default value)
+
+Company Properties
+------------------
+
+For understanding which values are default and which are company dependent do as following:
+
+* `Activate Developer Mode <https://odoo-development.readthedocs.io/en/latest/odoo/usage/debug-mode.html>`__
+* Open menu ``[[ Settings ]] >> Technical >> Parameters >> Company Properties``
+* Click ``[Group By] -> Field``
+* Now you can find all records under ``Value (ir.config_parameter)``
+
+Uninstallation
+==============
+
+* Uninstallation is quite risky! Be very carefull to do it on production!
+* Make database backup
+* Open menu ``[[ Settings ]] >> Technical >> Parameters >> System Parameters``
+* Make Export of all records (``[Action] -> Export``) -- exporting only column ``value`` is enough
+* Click ``[Import]`` button
+* Upload ``*.csv`` file
+* Click ``[Validate]`` -- it must not return errors!
+* Don't close current page!
+* At other page:
+
+  * uninstall module
+  * Upgrade ``base`` module
+
+* Return back to page with importing
+* Click ``[Import]``
+
---
+++ ir_config_parameter_multi_company/models/ir_config_parameter.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import models, fields, api, _, tools
+from odoo.addons.base.ir.ir_config_parameter import IrConfigParameter as IrConfigParameterOriginal
+
+_logger = logging.getLogger(__name__)
+PROP_NAME = _('Default value for "%s"')
+
+
+class IrConfigParameter(models.Model):
+    _inherit = 'ir.config_parameter'
+
+    value = fields.Text(company_dependent=True)
+
+    @api.model
+    def create(self, vals):
+        res = super(IrConfigParameter, self).create(vals)
+        # make value company independent
+        domain = self.env['ir.property']._get_domain('value', self._name)
+        domain += [('res_id', '=', '%s,%s' % (self._name, res.id))]
+        prop = self.env['ir.property'].search(domain)
+        prop.company_id = None
+        prop.name = PROP_NAME % res.key
+        return res
+
+    @api.model
+    def get_param(self, key, default=False):
+        company_id = self.env.context.get('company_id')
+        if not company_id:
+            website_id = self.env.context.get('website_id')
+            if website_id:
+                website = self.env['website'].browse(website_id)
+                company_id = website.company_id and website.company_id.id
+
+        if not company_id:
+            company_id = self.env.user.company_id.id
+
+        return super(IrConfigParameter, self.with_context(force_company=company_id)).get_param(key, default)
+
+    @api.model
+    @tools.ormcache_context('self._uid', 'key', keys=("force_company",))
+    def _get_param(self, key):
+        _logger.debug('_get_param(%s) context: %s', key, self.env.context)
+        # call undecorated super method. See odoo/tools/cache.py::ormcache and http://decorator.readthedocs.io/en/stable/tests.documentation.html#getting-the-source-code
+        return IrConfigParameterOriginal._get_param.__wrapped__(self, key)
+
+    @api.multi
+    def _create_default_value(self, value):
+        """Set company-independent default value"""
+        self.ensure_one()
+        self.env['ir.property'].create({
+            'fields_id': self.env.ref('base.field_ir_config_parameter_value').id,
+            'res_id': 'ir.config_parameter,%s' % self.id,
+            'name': PROP_NAME % self.key,
+            'value': value,
+            'type': 'text',
+        })
+
+    def _auto_init(self):
+        # Check that we have an value column
+        cr = self.env.cr
+        cr.execute("select COUNT(*) from information_schema.columns where table_name='ir_config_parameter' AND column_name='value';")
+        res = cr.dictfetchone()
+        if res.get('count'):
+            _logger.info('Starting conversion for ir.config_parameter: saving data for further processing.')
+            # Rename image column so we don't lose images upon module install
+            cr.execute("ALTER TABLE ir_config_parameter RENAME COLUMN value TO value_old")
+        else:
+            _logger.debug('No value field found in ir_config_parameter; no data to save.')
+        return super(IrConfigParameter, self)._auto_init()
+
+    def _auto_end(self):
+        super(IrConfigParameter, self)._auto_end()
+        cr = self.env.cr
+        # Only proceed if we have the appropriate _old field
+        cr.execute("select COUNT(*) from information_schema.columns where table_name='ir_config_parameter' AND column_name='value_old';")
+        res = cr.dictfetchone()
+        if not res.get('count'):
+            _logger.debug('No value_old field present in ir_config_parameter; assuming data is already saved in the filestore.')
+            return
+
+        _logger.info('Starting rewrite of ir.config_parameter, saving values to ir.property.')
+        for r in self.env['ir.config_parameter'].sudo().search([]):
+            cr.execute("SELECT key,value_old FROM ir_config_parameter WHERE id = %s", (r.id, ))
+            res = cr.dictfetchone()
+            value_old = res.get('value_old')
+            r._create_default_value(value_old)
+        # Finally, remove the _old column if all went well so we won't run this every time we upgrade the module.
+        cr.execute("ALTER TABLE ir_config_parameter DROP COLUMN value_old")
