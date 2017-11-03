1f2961b746923a5b4ccd4a82f6b81d8746e23ae0
Ivan Yelizariev <yelizariev@it-projects.info>
2017-11-03 18:25:01 +0500

    [PORT] attachment_large_object is used in docker and must be set as installable

--- attachment_large_object/__openerp__.py
+++ attachment_large_object/__openerp__.py
@@ -30,7 +30,7 @@
     'website': 'anybox.fr',
     'depends': ['base'],
     'test': [],
-    'installable': False,
+    'installable': True,
     'application': False,
     'auto_install': False,
     'license': 'AGPL-3',
