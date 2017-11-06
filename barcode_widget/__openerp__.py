9571bfbcf650ddc29a31d8765119c475d8a2cb84
George Daramouskas <daramousk@users.noreply.github.com>
2017-11-06 08:01:30 +0100

    [8.0][BACKPORT]  barcode_widget  (#491)

---
+++ barcode_widget/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Barcode Widget',
+    'version': '1.0',
+    'category': 'Widget',
+    'description': """
+    Feature:
+        - Barcode widget for form view
+
+    How to use:
+        - Adding widget="BarCode128" attribute for your Char field on view
+        Ex: <field name="barcode" widget="BarCode128" />
+
+    """,
+    'author': 'The Gok Team',
+    'depends': [
+        'web',
+    ],
+
+    'data': [
+        'views/assets.xml'
+    ],
+
+    'qweb': ['static/src/xml/*.xml'],
+    'js': [],
+    'test': [],
+    'demo': [],
+
+    'installable': True,
+    'active': False,
+    'application': True,
+}
