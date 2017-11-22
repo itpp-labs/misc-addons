1170ac352afd04b845e6625d900ef5735db1976f
Ivan Yelizariev <yelizariev@it-projects.info>
2017-11-22 10:08:51 +0000

    [IMP] update base module automatically

--- ir_config_parameter_multi_company/__init__.py
+++ ir_config_parameter_multi_company/__init__.py
@@ -9,3 +9,6 @@ def uninstall_hook(cr, registry):
     # remove properties
     field_id = env.ref('base.field_ir_config_parameter_value').id
     env['ir.property'].search([('fields_id', '=', field_id)]).unlink()
+
+    # update base module
+    env['ir.module.module'].search([('name', '=', 'base')]).button_upgrade()
