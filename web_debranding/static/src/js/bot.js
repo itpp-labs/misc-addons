b7c3aace11b661bb0a8fd6d47ef0b5ae5904e7be
Ivan Yelizariev <yelizariev@it-projects.info>
2017-11-23 16:04:13 +0000

    [11.0][PORT] removing of enterprise labels in Settings

--- web_debranding/static/src/js/bot.js
+++ web_debranding/static/src/js/bot.js
@@ -2,14 +2,14 @@ odoo.define('web_debranding.bot', function (require) {
     "use strict";
 
     require('web_debranding.dialog');
-    var ODOOBOT_ID = "ODOOBOT";
     var chat_manager = require('mail.chat_manager');
     var session = require('web.session');
+    var ODOOBOT_ID = "ODOOBOT";
 
     var make_message_super = chat_manager.make_message;
     chat_manager.make_message = function(data){
             var msg = make_message_super(data);
-            if (msg.author_id === mail_base.ODOOBOT_ID) {
+            if (msg.author_id === ODOOBOT_ID) {
                 msg.avatar_src = '/web/binary/company_logo?company_id=' + session.company_id;
                 msg.displayed_author = 'Bot';
             }
--- web_debranding/static/src/js/field_upgrade.js
+++ web_debranding/static/src/js/field_upgrade.js
@@ -1,47 +1,39 @@
 odoo.define('web_debranding.field_upgrade', function (require) {
 "use strict";
 
-    var core = require('web.core');
-    var UpgradeBoolean = core.form_widget_registry.get('upgrade_boolean');
-    var UpgradeRadio = core.form_widget_registry.get('upgrade_radio');
+    var field_registry = require('web.field_registry');
+    require('web.upgrade_widgets');
+    var UpgradeBoolean = field_registry.get('upgrade_boolean');
+    var UpgradeRadio = field_registry.get('upgrade_radio');
 
-    if (!UpgradeBoolean || UpgradeBoolean.prototype.template !== 'FieldUpgradeBoolean'){
+    if (!UpgradeBoolean){
         // we are on enterprise. No need to update
         return;
     }
 
     var include = {
-        'start': function(){
-            // $el = .oe_form_field
-            var $el = this.$el;
+        _render: function () {
+/*
+  Remove following element:
 
-            // $el = div
-            $el = $el.parent();
-            $el.hide();
+ <div class="col-xs-12 col-md-6 o_setting_box" title="Boost your sales with two kinds of discount programs: promotions and coupon codes. Specific conditions can be set (products, customers, minimum purchase amount, period). Rewards can be discounts (% or amount) or free products.">
+  <div class="o_setting_left_pane">
+    <div class="o_field_boolean o_field_widget" name="module_sale_coupon">
+   </div>
+  </div>
+<div class="o_setting_right_pane">
+  <label class="o_form_label" for="o_field_input_18" data-original-title="" title="" aria-describedby="tooltip822540">Coupons &amp; Promotions
+  </label>
+  <div class="text-muted" id="sale_coupon">
+                  Manage promotion &amp; coupon programs
 
-            // $el = div
-            $el = $el.parent();
-            if (_.all($el.children(), function (ch) {
-                        return $(ch).css('display') === 'none';
-                        }
-                    )
-                ){
-                // hide whole group as doesn't have fields
-                for (var i=0; i<10; i++) {
-                    if ($el.prop("tagName") === 'TR'){
-                        $el.hide();
-                        break;
-                    }
-                    $el = $el.parent();
-                    if (!$el) {
-                        break;
-                    }
-                }
-            }
-            return this._super.apply(this, arguments);
-        },
-        'on_click_input':function(){
-            console.log('skip');
+  </div>
+</div>
+</div>
+
+*/
+            // this.$el is .o_field_boolean in example above
+            this.$el.parent().parent().remove();
         }
     };
 
