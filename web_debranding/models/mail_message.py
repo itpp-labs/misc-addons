27da2287f88f258b828a38e4e6fa8436b10a6235
Your Name <kolushov@it-projects.info>
2017-12-21 17:20:06 +0500

    [REF] detalization of the condition for mail debranding

--- web_debranding/models/mail_message.py
+++ web_debranding/models/mail_message.py
@@ -14,6 +14,11 @@ class MailMessage(models.Model):
 
     def create(self, values):
         msg = super(MailMessage, self).create(values)
-        if type(msg.subject) is unicode and 'application installed' in msg.subject:
+        if type(msg.subject) is unicode and \
+           type(msg.channel_ids.description) is unicode and \
+           type(msg.channel_ids.name) is unicode and \
+           'application installed!' in msg.subject and \
+           'General announcements for all employees.' == msg.channel_ids.description and \
+           'general' == msg.channel_ids.name:
             msg.body = debrand(self.env, msg.body, is_code=False)
         return msg
