f0a2094781249eff4d52391b99d8f4354ac9a889
Ildar Nasyrov <nasyrov@it-projects.info>
2018-01-23 11:28:51 +0000

    [DOC] ir_attachment_url

--- ir_attachment_url/__init__.py
+++ ir_attachment_url/__init__.py
@@ -6,6 +6,6 @@ def post_load():
     from .models import binary_fields
     from .models import image
     # in /controllers/main there is an import from the `mail` module
-    # it leads to loading the mail module earlier than the patch in `binary_fields` is applied.
+    # it leads to loading the mail and all its dependencies earlier than the patch in `binary_fields` is applied.
     # That is why the `from . import controller` line is here
     from . import controllers
