cb031f46a19161627f5512531f1770dd84487665
GabbasovDinar <gabbasov@it-projects.info>
2018-01-24 18:16:45 +0500

    [LINT] fix lint error

--- project_timelog/tests/test_default.py
+++ project_timelog/tests/test_default.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from openerp.tests import HttpCase
+import openerp.tests
+
 
 @openerp.tests.common.at_install(True)
 @openerp.tests.common.post_install(True)
-class TestUi(HttpCase):
+class TestUi(openerp.tests.HttpCase):
 
     def test_01_check_timer(self):
         self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('project_timelog', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.project_timelog", login="admin")
