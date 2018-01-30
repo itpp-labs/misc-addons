28ca7d22cdf5d1fcb66d8624105723998aef5e31
Railia <34434651+Railia@users.noreply.github.com>
2018-01-30 02:47:01 -0900

    Update __manifest__.py

--- theme_kit/__manifest__.py
+++ theme_kit/__manifest__.py
@@ -3,7 +3,7 @@
     "name": """Brand kit""",
     "summary": """Brand your odoo instance in few clicks""",
     "category": "Debranding",
-    "live_test_url": "http://apps.it-projects.info/shop/product/theme_kit?version=11.0",
+    "live_test_url": "http://apps.it-projects.info/shop/product/theme-kit?version=11.0",
     "images": ['images/brandkit.png'],
     "version": "1.1.2",
     "application": False,
