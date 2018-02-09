9332b4144f36a663d696bee9a8abb5a50d60b9bf
Ivan Yelizariev <yelizariev@it-projects.info>
2018-02-09 16:38:59 +0500

    [LINT] fix imports

--- web_debranding/models/ir_translation.py
+++ web_debranding/models/ir_translation.py
@@ -5,7 +5,7 @@ from odoo import api
 from odoo import models
 from odoo import tools
 
-from .ir_config_parameter import PARAMS, get_debranding_parameters_env
+from .ir_config_parameter import get_debranding_parameters_env
 
 
 def debrand_documentation_links(source, new_documentation_website):
