68714f315360dde24b9f8d1ac8e37dd436498aac
Ivan Yelizariev <yelizariev@it-projects.info>
2018-02-13 22:54:43 +0500

    [DOC] bump 1.0.22 + new version format

--- web_debranding/__manifest__.py
+++ web_debranding/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Backend debranding",
-    'version': '1.0.20',
+    'version': '10.0.1.0.22',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     'category': 'Debranding',
