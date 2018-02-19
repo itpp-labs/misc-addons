41b1f1589dbefb7b180ece0e983d8fd23a06674a
manawi <manaewich@gmail.com>
2018-02-19 20:46:49 +0500

    [FIX] docs and manifest

--- reminder_task_deadline/__manifest__.py
+++ reminder_task_deadline/__manifest__.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Reminders and Agenda for Tasks",
-    'version': '1.0.0',
-    'author': 'IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko',
-    'license': 'LGPL-3',
-    'category': 'Reminders and Agenda',
-    'live_test_url': 'http://apps.it-projects.info/shop/product/reminders-and-agenda?version=10.0',
-    'website': 'https://twitter.com/yelizariev',
-    'price': 21.00,
-    'currency': 'EUR',
-    'depends': ['reminder_base', 'project'],
-    'data': [
-        'views/reminder_task_deadline_views.xml',
+    "name": "Reminders and Agenda for Tasks",
+    "summary": """Allows you to create reminders for tasks.""",
+    "category": "Reminders and Agenda",
+    # "live_test_url": "",
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko",
+    "support": "apps@it-projects.info",
+    "website": "https://twitter.com/yelizariev",
+    "license": "LGPL-3",
+    "price": 21.00,
+    "currency": "EUR",
+
+    "depends": [
+        "reminder_base",
+        "project",
     ],
-    'installable': True,
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/reminder_task_deadline_views.xml",
+    ],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
 }
