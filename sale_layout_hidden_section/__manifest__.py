a90339fbe15a937be32834d158a88099da2dacc2
Ilmir <ilmir.karamov@gmail.com>
2018-04-13 14:41:23 +0500

    [ADD] copyrights

--- sale_layout_hidden_section/__manifest__.py
+++ sale_layout_hidden_section/__manifest__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Artyom Losev
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": """Hidden Layouts for Sale Orders""",
     "summary": """
