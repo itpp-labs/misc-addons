390c4362f52cd56e82d20a297c7c89bb36c943bc
GabbasovDinar <gabbasov@it-projects.info>
2018-06-18 17:17:18 +0500

    [CI] fix test

--- project_timelog/tests/test_default.py
+++ project_timelog/tests/test_default.py
@@ -7,4 +7,4 @@ import openerp.tests
 class TestUi(openerp.tests.HttpCase):
 
     def test_01_check_timer(self):
-        self.phantom_js("/", "odoo.__DEBUG__.services['web.Tour'].run('project_timelog', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.project_timelog", login="admin")
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web.Tour'].run('project_timelog', 'test')", "odoo.__DEBUG__.services['web.Tour'].tours.project_timelog", login="admin")
