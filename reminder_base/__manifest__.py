9233aaef7cc52fc1f95990af33780b01173160c7
Ildar Nasyrov <nasyrov@it-projects.info>
2018-07-04 08:25:41 +0000

    :book: reminder_base: fix version

--- reminder_base/__manifest__.py
+++ reminder_base/__manifest__.py
@@ -3,7 +3,7 @@
     "category": "Reminders and Agenda",
     'live_test_url': 'http://apps.it-projects.info/shop/product/reminders-and-agenda?version=10.0',
     "images": [],
-    "version": "10.0.1.0.7",
+    "version": "11.0.1.0.7",
     "application": False,
 
     "author": "IT-Projects LLC, Ivan Yelizariev, Pavel Romanchenko",
