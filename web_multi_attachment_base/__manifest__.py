244f414d2031343ce60b8a278ffc9ea2b6e0cdda
Aleksandr <kolushov@it-projects.info>
2018-08-15 14:07:39 +0500

    :tada::one::one: web_multi_attachment_base module (it-projects-llc/addons-dev#538)

---
+++ web_multi_attachment_base/__manifest__.py
@@ -0,0 +1,39 @@
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """Upload Multiple Image Attachments at Once""",
+    "summary": """Upload multiple images at once""",
+    "category": "Extra Tools",
+    # "live_test_url": "",
+    "images": [],
+    "version": "11.0.1.0.0",
+    "application": False,
+
+    "author": "IT-Projects LLC, Dinar Gabbasov, Kolushov Alexandr",
+    "support": "apps@it-projects.info",
+    "website": "https://it-projects.info/team/GabbasovDinar",
+    "license": "LGPL-3",
+    # "price": 9.00,
+    # "currency": "EUR",
+
+    "depends": [
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        "views/template.xml",
+    ],
+    "qweb": [
+        "static/src/xml/web_kanban.xml"
+    ],
+    "demo": [
+    ],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
