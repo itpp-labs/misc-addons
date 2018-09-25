2d1be7f582424a945503b2412e79a00f921c0d1a
Dinar <gabbasov@it-projects.info>
2018-09-25 16:07:29 +0500

    :rainbow: CI errors (#662)

--- theme_kit/__manifest__.py
+++ theme_kit/__manifest__.py
@@ -1,11 +1,13 @@
-
+# Copyright 2016 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": """Brand kit""",
     "summary": """Brand your odoo instance in few clicks""",
     "category": "Debranding",
     "live_test_url": "http://apps.it-projects.info/shop/product/theme-kit?version=11.0",
     "images": ['images/brandkit.png'],
-    "version": "1.1.2",
+    "version": "11.0.1.1.2",
     "application": False,
 
     "author": "IT-Projects LLC, Ivan Yelizariev",
