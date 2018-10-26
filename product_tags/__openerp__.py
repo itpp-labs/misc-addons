cdd1bf638478bde1e994f69fba16772a223ba0f1
Denis Mudarisov <mudarisov@it-projects.info>
2018-10-26 18:28:19 +0500

    :ambulance: can't use child_of operator with many2many fields (#670)

--- product_tags/__openerp__.py
+++ product_tags/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     "name": "Product Tags",
-    "version": "1.0",
+    "version": "10.0.1.0.1",
     "author": "Julius Network Solutions",
     "website": "http://julius.fr",
     "category": "Sales Management",
