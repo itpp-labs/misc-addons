493c5ef71aacd8e74a71adbf3ef8ad4e4279aa5d
Rafis Bikbov <bikbov@it-projects.info>
2018-11-02 19:21:47 +0500

    :ambulance: when the "image_resize_image" function was called, they received the error "binascii.Error: decoding with base64 codec failed (Error: Incorrect padding)", since the value of the binary field is the URL, not the base_64 string

--- ir_attachment_url/__manifest__.py
+++ ir_attachment_url/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """Use attachment URL and upload data to external storage""",
     "category": "Tools",
     "images": [],
-    "version": "11.0.1.1.5",
+    "version": "11.0.1.1.6",
     "application": False,
 
     "author": "IT-Projects LLC, Ildar Nasyrov",
