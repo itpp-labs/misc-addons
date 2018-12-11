5fd8495ae91d377a67d7492115c5a14969a0bfb9
Rafis Bikbov <bikbov@it-projects.info>
2018-12-11 16:10:22 +0500

    :ambulance: 'guess_type' does not always return a result so an 'IncorrectPadding' error occurs

--- ir_attachment_url/models/binary_fields.py
+++ ir_attachment_url/models/binary_fields.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
-from odoo import fields
 import mimetypes
+import requests
+
+from odoo import fields
+from odoo.tools.mimetypes import guess_mimetype
 from . import image
+
+
+def get_mimetype_and_optional_content_by_url(url):
+    mimetype = mimetypes.guess_type(url)[0]
+    content = None
+
+    # head request for content-type header getting
+    if not mimetype:
+        with requests.head(url) as r:
+            mimetype = getattr(r, 'headers', {}).get('Content-Type')
+
+    index_content = mimetype and mimetype.split('/')[0]
+    if not mimetype or index_content == 'text':
+        with requests.get(url) as r:
+            content = getattr(r, 'content')
+            if not mimetype and content:
+                mimetype = guess_mimetype(content)
+
+    return mimetype, content
 
 
 class Binary(fields.Binary):
@@ -21,13 +43,18 @@ class Binary(fields.Binary):
         if value and image.is_url(value):
             with records.env.norecompute():
                 if value:
+                    mimetype, content = get_mimetype_and_optional_content_by_url(value)
+                    index_content = records.env['ir.attachment']._index(content, None, mimetype)
+
                     # update the existing attachments
                     atts.write({
                         'url': value,
-                        'mimetype': mimetypes.guess_type(value)[0],
+                        'mimetype': mimetype,
                         'datas': None,
                         'type': 'url',
+                        'index_content': index_content,
                     })
+
                     # create the missing attachments
                     for record in (records - records.browse(atts.mapped('res_id'))):
                         atts.create({
@@ -37,6 +64,8 @@ class Binary(fields.Binary):
                             'res_id': record.id,
                             'type': 'url',
                             'url': value,
+                            'mimetype': mimetype,
+                            'index_content': index_content,
                         })
                 else:
                     atts.unlink()
