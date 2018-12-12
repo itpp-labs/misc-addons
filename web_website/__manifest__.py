4f1ca5a0119a1bb17e8b1fdda591204d34e2dc12
KolushovAlexandr <kolushov@it-projects.info>
2018-12-12 15:02:43 +0500

    :ambulance: minor fix after merging 11 to 12

--- web_website/__manifest__.py
+++ web_website/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": """Website Switcher in Backend""",
