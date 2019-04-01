7ff6ccc8ebd0692a53bfd26c63284c3adce285b1
ommo73 <rafailov@it-projects.info>
2019-04-01 14:41:05 +0500

    :arrow_up::one::two: autostaging_base Port

--- autostaging_base/README.rst
+++ autostaging_base/README.rst
@@ -1,5 +1,49 @@
-Autostaging (technical core)
-============================
+.. image:: https://img.shields.io/badge/license-LGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/lgpl
+   :alt: License: LGPL-3
 
+==================
+ Autostaging base
+==================
 
-Tested on Odoo 8.0 a0797d3b35cc235048e7947dd7a3d38e18c3e350
+Change stages of records automatically after a specified time
+
+The records which are not modified within the idle timeout automatically moved to the next stage
+
+Credits
+=======
+
+Contributors
+------------
+* 'Ildar Nasyrov <iledarn@it-projects.info>'__
+
+Sponsors
+--------
+* `IT-Projects LLC <https://it-projects.info>`__
+
+Maintainers
+-----------
+* `IT-Projects LLC <https://it-projects.info>`__
+
+      To get a guaranteed support
+      you are kindly requested to purchase the module
+      at `odoo apps store <https://apps.odoo.com/apps/modules/12.0/autostaging_base/>`__.
+
+      Thank you for understanding!
+
+      `IT-Projects Team <https://www.it-projects.info/team>`__
+
+Further information
+===================
+
+Demo: http://runbot.it-projects.info/demo/misc-addons/12.0
+
+HTML Description: https://apps.odoo.com/apps/modules/12.0/autostaging_base/
+
+Usage instructions: `<doc/index.rst>`_
+
+Changelog: `<doc/changelog.rst>`_
+
+Notifications on updates: `via Atom <https://github.com/it-projects-llc/misc-addons/commits/12.0/autostaging_base.atom>`_, `by Email <https://blogtrottr.com/?subscribe=https://github.com/it-projects-llc/misc-addons/commits/12.0/autostaging_base.atom>`_
+
+Tested on Odoo 12.0 1e6f47ae56efe22101aa3db6159cd3cc16b9c120
--- autostaging_base/models.py
+++ autostaging_base/models.py
@@ -1,14 +1,19 @@
-
-from openerp import models, fields, api
+# Copyright 2015 Ildar Nasyrov <https://it-projects.info/>
+# Copyright 2016 x620 <https://github.com/x620>
+# Copyright 2017-2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2019 Artem Rafailov <https://it-projects.info/team/Ommo73/>
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl.html).
+from odoo import models, fields, api
 import datetime
 import time
-from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from openerp.exceptions import ValidationError
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
 
 
 class AutostagingFolder(models.AbstractModel):
     _name = 'autostaging.folder'
+    _description = 'autostaging_folder'
     autostaging_enabled = fields.Boolean('Autostaging enabled', default=True)
 
 
@@ -16,6 +21,7 @@ class AutostagingStage(models.AbstractModel):
     _name = 'autostaging.stage'
     _card_model = 'define_some_card_model'
     _card_stage_id = 'define_some_card_stage_id'
+    _description = 'autostaging_stage'
     autostaging_idle_timeout = fields.Integer('Autostagint idle timeout')
     autostaging_enabled = fields.Boolean('Autostaging enabled', default=False)
     # should be defined on inherired model:
@@ -43,6 +49,7 @@ class AutostagingCard(models.AbstractModel):
     _name = 'autostaging.card'
     _field_folder_id = 'define_some_field_folder_id'
     _field_stage_id = 'define_some_field_stage_id'
+    _description = 'autostaging_card'
 
     autostaging_date = fields.Date(string='Autostaging date', readonly=True)
     autostaging_days_left = fields.Integer(string='Days left', compute='_compute_autostaging_days_left')
@@ -67,10 +74,9 @@ class AutostagingCard(models.AbstractModel):
     @api.multi
     def _get_autostaging_date(self):
         self.ensure_one()
-
         delta = datetime.timedelta(days=getattr(self, self._field_stage_id).autostaging_idle_timeout)
         return (datetime.datetime.strptime(
-            self.write_date, DEFAULT_SERVER_DATETIME_FORMAT) + delta).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+            str(self.write_date)[:-7], DEFAULT_SERVER_DATETIME_FORMAT) + delta).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
 
     @api.multi
     def _update_autostaging_date(self):
@@ -106,13 +112,13 @@ class AutostagingCard(models.AbstractModel):
     def _compute_autostaging_days_left_one(self):
         self.ensure_one()
         today = datetime.datetime.now()
-        date_modifications = datetime.datetime.strptime(self.write_date, DEFAULT_SERVER_DATETIME_FORMAT)
+        date_modifications = datetime.datetime.strptime(str(self.write_date)[:-7], DEFAULT_SERVER_DATETIME_FORMAT)
         delta = today - date_modifications
         self.autostaging_days_left = getattr(self, self._field_stage_id).autostaging_idle_timeout - delta.days
 
     def _get_model_list(self):
         res = []
-        for r in self.env['ir.model.fields'].search([('name', '=', 'next_stage_related')]):
+        for r in self.env['ir.model.fields'].search([('name', '=', 'autostaging_card_next_stage')]):
             res.append(r.model_id.model)
         return res
 
@@ -128,9 +134,9 @@ class AutostagingCard(models.AbstractModel):
                   (self._field_folder_id, '=', False),
                   '&', '&',
                   ((self._field_stage_id + '.autostaging_enabled'), '=', True),
-                  ((self._field_stage_id + '.next_stage'), '!=', False),
+                  ((self._field_stage_id + '.autostaging_next_stage'), '!=', False),
                   ('autostaging_date', '<=', time.strftime('%Y-%m-%d'))]
         cards = self.search(domain)
         for card in cards:
             card.with_context(autostaging=True).write(
-                {self._field_stage_id: getattr(card, self._field_stage_id).next_stage.id})
+                {self._field_stage_id: getattr(card, self._field_stage_id).autostaging_next_stage.id})
