86188b300e8decae123d5db0c2deeade7b9f39e3
ommo73 <rafailov@it-projects.info>
2019-08-10 14:27:32 +0500

    :shield: res_partner_skype: corrected tour

--- res_partner_skype/static/src/js/test_res_partner_skype.js
+++ res_partner_skype/static/src/js/test_res_partner_skype.js
@@ -10,6 +10,7 @@ odoo.define('res_partner_skype.tour', function (require) {
             trigger: '.o_field_email.o_field_widget.o_form_uri.o_text_overflow[href="skype:skype_test?call"]',
             content: _t("Check the link"),
             position: 'bottom',
+            timeout: 60000,
         }];
 
     tour.register('tour_res_partner_skype', { test: true, url: '/web#id=9&view_type=form&model=res.partner' }, steps);
