5c07e2fa524125d9e33cba5fa58933859ceeaa8d
Ivan Yelizariev <yelizariev@it-projects.info>
2019-10-02 17:19:56 +0500

    extra commit to restart runbot

--- base_details/static/src/js/FieldReference.js
+++ base_details/static/src/js/FieldReference.js
@@ -7,7 +7,7 @@ odoo.define('base_details', function(require) {
         _reset: function () {
             this._super.apply(this, arguments);
             // due to odoo client error we've redefined this method to be able to change reference field values after it was set in onchange method src: https://github.com/odoo/odoo/pull/27109
-            //this._setState();
+            //this._setState(); 
         },
     });
 
