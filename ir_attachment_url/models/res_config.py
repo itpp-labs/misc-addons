795c9cc96b54e21293103c439a319ae8db6a1048
Eugene Molotov <molotov@it-projects.info>
2019-10-11 11:01:18 +0000

    :rainbow: ir_attachment_url: take away unused method set_ir_attachment_url

--- ir_attachment_url/models/res_config.py
+++ ir_attachment_url/models/res_config.py
@@ -14,13 +14,6 @@ class IrAttachmentSettings(models.TransientModel):
         required=True,
     )
 
-    def set_ir_attachment_url_storage(self):
-        self.env['ir.config_parameter'].set_param(
-            "ir_attachment_url.storage",
-            self.ir_attachment_url_storage or 'url',
-            groups=['base.group_system'],
-        )
-
     def get_default_all(self, fields):
         ir_attachment_url_storage = self.env["ir.config_parameter"].get_param("ir_attachment_url.storage", default='url')
         return dict(
