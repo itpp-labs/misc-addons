67a325b59741f60bdbf3ec3d641ff8f8171f042f
Eugene Molotov <molotov@it-projects.info>
2019-10-18 11:40:40 +0000

    :book: ir_attachment_url: added changelog entry

--- ir_attachment_url/__manifest__.py
+++ ir_attachment_url/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """Use attachment URL and upload data to external storage""",
     "category": "Tools",
     "images": [],
-    "version": "10.0.1.1.7",
+    "version": "10.0.1.1.8",
     "application": False,
 
     "author": "IT-Projects LLC, Ildar Nasyrov",
