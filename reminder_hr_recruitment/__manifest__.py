664ec991889275032b41cec767cf127a363bb2c6
Artur Nigmatullin <nigmatullin@it-projects.info>
2019-10-19 20:28:53 +0500

    :euro:

--- reminder_hr_recruitment/__manifest__.py
+++ reminder_hr_recruitment/__manifest__.py
@@ -6,7 +6,7 @@
     'category': 'Reminders and Agenda',
     'live_test_url': 'http://apps.it-projects.info/shop/product/reminders-and-agenda?version=10.0',
     'website': 'https://twitter.com/yelizariev',
-    'price': 21.00,
+    'price': 11.00,
     'currency': 'EUR',
     'depends': ['reminder_base', 'hr_recruitment'],
     'data': [
