ba16a888921d5f62c047920f7f12578db4ebf705
Ivan Yelizariev <yelizariev@it-projects.info>
2019-11-04 10:56:58 +0500

    :tada::one::two: odoo_backup_sh [_dropbox, _google_disk]
    dev history can be found here https://github.com/it-projects-llc/addons-dev/commits/misc-addons-12.0-odoo_backup_sh

---
+++ odoo_backup_sh/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2018 Stanislav Krotov <https://it-projects.info/team/ufaks>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import test_odoo_backup_sh_auto_rotation
+from . import test_odoo_backup_sh_ui
