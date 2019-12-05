a3481aef69010f5cfc18b705cebb603052f18f22
Ilmir <ilmir.karamov@gmail.com>
2019-12-05 19:49:45 +0500

    :peace_symbol:

--- web_debranding/__manifest__.py
+++ web_debranding/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2015-2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # Copyright 2017 Ilmir Karamov <https://it-projects.info/team/ilmir-k>
-# Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
+# Copyright 2018-2019 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # Copyright 2018 Ildar Nasyrov <https://it-projects.info/team/iledarn>
 # Copyright 2018 WohthaN <https://github.com/WohthaN>
 # Copyright 2019 Eugene Molotov <https://github.com/em230418>
