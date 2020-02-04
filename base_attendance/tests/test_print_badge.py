77656467ffe6f30b5f957b483ca9865d9ddaf6c3
Eugene Molotov <molotov@it-projects.info>
2020-02-04 12:13:24 +0500

    :shield: base_attendance: make sure badge page returns 200 status code

---
+++ base_attendance/tests/test_print_badge.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License LGPL-3.0 (http://www.gnu.org/licenses/lgpl.html).
+
+import odoo.tests
+
+
+@odoo.tests.tagged('at_install', 'post_install')
+class TestPrintBadge(odoo.tests.HttpCase):
+
+    def test_print_badge(self):
+        self.authenticate("admin", "admin")
+
+        demo_user = self.env['res.users'].search([('login', '=', 'demo')])
+        self.assertEqual(len(demo_user), 1)
+
+        r = self.url_open('/report/html/base_attendance.print_partner_badge/{}'.format(demo_user.id))
+        self.assertEqual(r.status_code, 200)
