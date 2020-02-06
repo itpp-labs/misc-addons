643aa589a1d786a0870616eb75424dca43906d62
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-06 06:47:51 +0000

    :x: delete fleet_odometer_oil

--- fleet_odometer_oil/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import fields, models
-
-
-class FleetVehicle(models.Model):
-    _inherit = "fleet.vehicle"
-
-    oil_change_odometer = fields.Float("Last oil change (odometer)")
