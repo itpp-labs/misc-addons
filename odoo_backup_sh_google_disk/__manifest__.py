ba2e11142ed19125f5eb149f724a111bc1b579f5
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-07 08:31:12 +0000

    :green_heart: pre-commit: auto cleanups

--- odoo_backup_sh_google_disk/__manifest__.py
+++ odoo_backup_sh_google_disk/__manifest__.py
@@ -5,37 +5,28 @@
     "summary": """The small investment to protect your business""",
     "category": "Backup",
     # "live_test_url": "",
-    "images": ['images/google drive backing up.jpg'],
+    "images": ["images/google drive backing up.jpg"],
     "version": "13.0.1.0.0",
     "application": False,
-
     "author": "IT-Projects LLC, Dinar Gabbasov",
     "support": "apps@it-projects.info",
     "website": "https://apps.odoo.com/apps/modules/13.0/odoo_backup_sh_google_disk/",
     "license": "LGPL-3",
     "price": 119.00,
     "currency": "EUR",
-
-    "depends": [
-        "odoo_backup_sh",
-    ],
+    "depends": ["odoo_backup_sh"],
     "external_dependencies": {"python": ["google-api-python-client"], "bin": []},
     "data": [
         "views/odoo_backup_sh_views.xml",
         "views/odoo_backup_sh_google_drive_templates.xml",
         "views/res_config_settings_views.xml",
     ],
-    'qweb': [
-        "static/src/xml/dashboard.xml"
-    ],
-    "demo": [
-    ],
-
+    "qweb": ["static/src/xml/dashboard.xml"],
+    "demo": [],
     "post_load": None,
     "pre_init_hook": None,
     "post_init_hook": None,
     "uninstall_hook": None,
-
     "auto_install": False,
     "installable": True,
 }
