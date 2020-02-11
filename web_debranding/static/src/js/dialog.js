cbe3e82a8cae33862d385b6a08261d60141efee2
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-11 08:33:57 +0000

    :green_heart: web_debranding/static/src/js/dialog

--- web_debranding/static/src/js/dialog.js
+++ web_debranding/static/src/js/dialog.js
@@ -11,7 +11,8 @@ odoo.define("web_debranding.dialog", function(require) {
         init: function(parent, options) {
             var debranding_new_name = odoo.debranding_new_name;
             var debranding_new_website = odoo.debranding_new_website;
-            options = options || {};
+            if (!options)
+                return this._super(parent, options);
             if (options.title && options.title.replace) {
                 var title = options.title.replace(/Odoo/gi, debranding_new_name);
                 options.title = title;
@@ -30,7 +31,7 @@ odoo.define("web_debranding.dialog", function(require) {
                 content_html = content_html.replace(/Odoo/gi, debranding_new_name);
                 options.$content.html(content_html);
             }
-            this._super(parent, options);
+            return this._super(parent, options);
         },
     });
 });
