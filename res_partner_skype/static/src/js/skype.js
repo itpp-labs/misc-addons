579a820e7b6e0c92ea8bc7ac6aadc1c5c8473242
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-11 07:33:25 +0000

    :green_heart: pre-commit: auto cleanups

--- res_partner_skype/static/src/js/skype.js
+++ res_partner_skype/static/src/js/skype.js
@@ -1,25 +1,31 @@
-odoo.define('res_partner_skype.widget', function (require) {
-'use strict';
+odoo.define("res_partner_skype.widget", function(require) {
+    "use strict";
 
-var core = require('web.core');
-var FieldChar = core.form_widget_registry.get('char');
+    var core = require("web.core");
+    var FieldChar = core.form_widget_registry.get("char");
 
-var FieldSkype = FieldChar.extend({
-    template: 'FieldSkype',
-    prefix: 'skype',
-    init: function() {
-        this._super.apply(this, arguments);
-        this.clickable = true;
-    },
+    var FieldSkype = FieldChar.extend({
+        template: "FieldSkype",
+        prefix: "skype",
+        init: function() {
+            this._super.apply(this, arguments);
+            this.clickable = true;
+        },
 
-    render_value: function() {
-        this._super();
-        if (this.get("effective_readonly") && this.clickable) {
-            this.$el.attr('href', this.prefix + ':' + this.get('value') + '?'+(this.options.type || 'call'));
-        }
-    }
-});
+        render_value: function() {
+            this._super();
+            if (this.get("effective_readonly") && this.clickable) {
+                this.$el.attr(
+                    "href",
+                    this.prefix +
+                        ":" +
+                        this.get("value") +
+                        "?" +
+                        (this.options.type || "call")
+                );
+            }
+        },
+    });
 
-core.form_widget_registry.add('skype', FieldSkype);
-
+    core.form_widget_registry.add("skype", FieldSkype);
 });
--- web_debranding/static/src/js/user_menu.js
+++ web_debranding/static/src/js/user_menu.js
@@ -1,19 +1,18 @@
-odoo.define('web_debranding.UserMenu', function (require) {
+odoo.define("web_debranding.UserMenu", function(require) {
     "use strict";
 
-    require('web_debranding.base');
-    var session = require('web.session');
-    var core = require('web.core');
+    require("web_debranding.base");
+    var session = require("web.session");
+    var core = require("web.core");
     var _t = core._t;
 
-
-    var UserMenu = require('web.UserMenu');
+    var UserMenu = require("web.UserMenu");
     UserMenu.include({
-        on_menu_debug: function(){
-            if (session.debug){
-                return console.log(_t('Developer mode is already activated'));
+        on_menu_debug: function() {
+            if (session.debug) {
+                return console.log(_t("Developer mode is already activated"));
             }
-            window.location = $.param.querystring(window.location.href, 'debug');
-        }
+            window.location = $.param.querystring(window.location.href, "debug");
+        },
     });
 });
