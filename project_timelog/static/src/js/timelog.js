50659bb1f65633128041d03eed66b652eb1777a1
Vildan Safin <safin@it-projects.info>
2020-02-12 15:01:48 +0500

    :rainbow: pre-commit: project_timelog cleanup
    Signed-off-by: Vildan Safin <safin@it-projects.info>

--- project_timelog/static/src/js/timelog.js
+++ project_timelog/static/src/js/timelog.js
@@ -1,4 +1,5 @@
 odoo.define("project_timelog.timelog", function(require) {
+    "use strict";
     var bus = require("bus.bus");
     var session = require("web.session");
     var Widget = require("web.Widget");
@@ -6,7 +7,6 @@ odoo.define("project_timelog.timelog", function(require) {
     var Model = require("web.Model");
     var core = require("web.core");
     var _t = core._t;
-    var ActionManager = require("web.ActionManager");
     var TimeLog = {};
 
     // Prevent bus to be started by chat_manager.js
