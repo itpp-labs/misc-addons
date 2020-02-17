345ed407e71e92d0445c4f9e820f7a5111b86555
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-17 13:04:04 +0500

    :book: Distribute under MIT license
    If you made a contribution and don't agree with this change please open an issue

--- odoo_backup_sh/__init__.py
+++ odoo_backup_sh/__init__.py
@@ -1,6 +1,6 @@
 # Copyright 2018 Stanislav Krotov <https://it-projects.info/team/ufaks>
 # Copyright 2019 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License MIT (https://opensource.org/licenses/MIT).
 
 from . import controllers
 from . import models
--- odoo_backup_sh/models/res_config_settings.py
+++ odoo_backup_sh/models/res_config_settings.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
 # Copyright 2019 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License MIT (https://opensource.org/licenses/MIT).
 
 from odoo import api, fields, models
 
