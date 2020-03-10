fca0b5ed0c44c857b4ca1b851b74f430bd58cfbf
Eugene Molotov <molotov@it-projects.info>
2020-03-10 10:53:42 +0500

    :ambulance: ir_attachment_url: URL storage settings could not be changed via web UI

--- ir_attachment_url/models/res_config.py
+++ ir_attachment_url/models/res_config.py
@@ -1,9 +1,9 @@
 # Copyright 2019 Rafis Bikbov <https://it-projects.info/team/RafiZz>
 # Copyright 2019 Alexandr Kolushov <https://it-projects.info/team/KolushovAlexandr>
-# Copyright 2019 Eugene Molotov <https://it-projects.info/team/em230418>
+# Copyright 2019-2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License MIT (https://opensource.org/licenses/MIT).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class IrAttachmentSettings(models.TransientModel):
@@ -17,8 +17,18 @@ class IrAttachmentSettings(models.TransientModel):
         required=True,
     )
 
-    def get_default_all(self, fields):
-        ir_attachment_url_storage = self.env["ir.config_parameter"].get_param(
-            "ir_attachment_url.storage", default="url"
+    def set_values(self):
+        super(IrAttachmentSettings, self).set_values()
+        self.env["ir.config_parameter"].set_param(
+            "ir_attachment_url.storage", self.ir_attachment_url_storage or "url",
         )
-        return dict(ir_attachment_url_storage=ir_attachment_url_storage)
+
+    @api.model
+    def get_values(self):
+        res = super(IrAttachmentSettings, self).get_values()
+        res.update(
+            ir_attachment_url_storage=self.env["ir.config_parameter"].get_param(
+                "ir_attachment_url.storage", default="url"
+            )
+        )
+        return res
