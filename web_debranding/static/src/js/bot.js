0ae682029d04239ad5495062a9940da1099a6d7d
Dennis Uhlemann <dennis.uhlemann@sirum.de>
2020-09-28 15:24:26 +0200

    FIX(web_debranding): bot.js made further inheritance impossible

--- web_debranding/static/src/js/bot.js
+++ web_debranding/static/src/js/bot.js
@@ -8,9 +8,9 @@ odoo.define("web_debranding.bot", function(require) {
     var session = require("web.session");
     var ODOOBOT_ID = "ODOOBOT";
 
-    var make_message_super = chat_manager.make_message;
+    chat_manager._make_message_debranding_bot_super = chat_manager.make_message;
     chat_manager.make_message = function(data) {
-        var msg = make_message_super(data);
+        var msg = this._make_message_debranding_bot_super(data);
         if (msg.author_id === ODOOBOT_ID) {
             msg.avatar_src =
                 "/web/binary/company_logo?company_id=" + session.company_id;
