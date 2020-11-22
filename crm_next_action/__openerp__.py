5c42781c7f63c8efc4b384062e16f134d38988e5
Ivan Yelizariev // IEL <yelizariev@itpp.dev>
2020-11-22 19:36:17 +0100

    Update __openerp__.py

--- crm_next_action/__openerp__.py
+++ crm_next_action/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Log\Notify Next Action changes',
+    'name': 'Log / Notify Next Action changes',
     'version': '1.0.0',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
