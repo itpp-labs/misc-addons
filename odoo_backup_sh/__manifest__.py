39e7a873baee3992e63e5b4472483cec653e1b6e
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-11-29 16:13:42 +0500

    :book: make module uninstallable after transferring commits

--- odoo_backup_sh/__manifest__.py
+++ odoo_backup_sh/__manifest__.py
@@ -35,7 +35,7 @@
     "pre_init_hook": None,
     "post_init_hook": None,
     "auto_install": False,
-    "installable": True,
+    "installable": False,
     "application": True,
     "uninstall_hook": "uninstall_hook",
 }
