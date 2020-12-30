67cad886bd3f9db6ecc3d298ef73b8a301beb660
Denis Mudarisov <denis.mudarisov@gmail.com>
2020-12-30 12:05:54 +0500

    :book: update copyright info

--- ir_attachment_url/models/ir_attachment.py
+++ ir_attachment_url/models/ir_attachment.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018 Ildar Nasyrov <https://it-projects.info/team/iledarn>
-# Copyright 2016-2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2016-2020 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
