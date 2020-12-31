9162ce5e63eb29a57b31df6c7d5c15b536daa859
Mitchell Admin <itpp-bot@users.noreply.github.com>
2020-12-31 17:10:16 +0000

    :tada::one::three: ir_attachment_s3 previous commits history: https://github.com/itpp-labs/misc-addons/commits/13.0/ir_rule_protected
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ ir_attachment_s3/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": """S3 Attachment Storage""",
+    "summary": """Upload attachments on Amazon S3""",
+    "category": "Tools",
+    "images": [],
+    "version": "13.0.2.1.0",
+    "application": False,
+    "author": "IT-Projects LLC, Ildar Nasyrov",
+    "website": "https://it-projects.info",
+    "license": "Other OSI approved licence",  # MIT
+    "price": 200.00,
+    "currency": "EUR",
+    "depends": ["base_setup", "ir_attachment_url"],
+    "external_dependencies": {"python": ["boto3"], "bin": []},
+    "data": ["data/ir_attachment_s3_data.xml", "views/res_config_settings_views.xml"],
+    "qweb": [],
+    "demo": [],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "auto_install": False,
+    "installable": True,
+}
