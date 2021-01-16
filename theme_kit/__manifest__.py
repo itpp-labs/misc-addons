8da6f98b729dadfb25a06582633b43156003aad9
Sengrith <psengrith@gmail.com>
2021-01-16 11:52:33 +0700

    [theme_kit] change 'web_widget_colorpicker' to 'web_widget_color'

--- theme_kit/__manifest__.py
+++ theme_kit/__manifest__.py
@@ -14,7 +14,7 @@
     "license": "Other OSI approved licence",  # MIT
     "price": 50.00,
     "currency": "EUR",
-    "depends": ["web_debranding", "web_login_background", "web_widget_colorpicker"],
+    "depends": ["web_debranding", "web_login_background", "web_widget_color"],
     "external_dependencies": {"python": [], "bin": []},
     "data": [
         "security/ir.model.access.csv",
