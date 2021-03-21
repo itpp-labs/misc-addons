0d7e990a2c02e8e1a72376ff544aa4972d0c515f
Ivan Yelizariev <iel@odoo.com>
2021-03-21 13:22:36 +0100

    :book: new website

--- np_stock_inventory/__openerp__.py
+++ np_stock_inventory/__openerp__.py
@@ -3,7 +3,7 @@
     'version' : '1.0.0',
     'author' : 'IT-Projects LLC, Ivan Yelizariev',
     'category' : 'Stock',
-    'website' : 'https://it-projects.info',
+    'website' : 'https://twitter.com/OdooFree',
     'description': """
 * In Physical Inventories "Fill inventory" button add only not listed products (by default line for same product is created if quantity are different from stock value)
 * add checkbox "inventoried" in stock.inventory.line
