7284a5615b215adb2a0643f6f318a7e2848b3833
Ivan Yelizariev <iel@odoo.com>
2021-12-08 07:38:57 +0100

    :x: Exodus of odoo_backup_sh

--- odoo_backup_sh/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2018 Stanislav Krotov <https://it-projects.info/team/ufaks>
-# Copyright 2019 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
-# License MIT (https://opensource.org/licenses/MIT).
-
-from . import odoo_backup_sh
-from . import res_config_settings
--- odoo_backup_sh/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2018 Stanislav Krotov <https://it-projects.info/team/ufaks>
-# License MIT (https://opensource.org/licenses/MIT).
-
-from . import test_odoo_backup_sh_auto_rotation
-from . import test_odoo_backup_sh_ui
